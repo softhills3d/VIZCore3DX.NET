3e4e4c6ef9b34071a74ff1040e997a294dd17b7f
Gwangjung, Kim <gjkim@softhills.net>
2024-05-21 15:48:58 +0900

    Effect 기능 추가

--- CoreExample/VIZCore3DX.NET.Demo/FrmMain.Designer.cs
+++ CoreExample/VIZCore3DX.NET.Demo/FrmMain.Designer.cs
@@ -30,6 +30,10 @@
         {
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.btnOpen = new System.Windows.Forms.Button();
+            this.ckEdge = new System.Windows.Forms.CheckBox();
+            this.ckSSAO = new System.Windows.Forms.CheckBox();
+            this.ckShadow = new System.Windows.Forms.CheckBox();
+            this.ckEnvLight = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
@@ -56,11 +60,59 @@
             this.btnOpen.UseVisualStyleBackColor = true;
             this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
             // 
+            // ckEdge
+            // 
+            this.ckEdge.AutoSize = true;
+            this.ckEdge.Location = new System.Drawing.Point(220, 16);
+            this.ckEdge.Name = "ckEdge";
+            this.ckEdge.Size = new System.Drawing.Size(60, 16);
+            this.ckEdge.TabIndex = 2;
+            this.ckEdge.Text = "모서리";
+            this.ckEdge.UseVisualStyleBackColor = true;
+            this.ckEdge.CheckedChanged += new System.EventHandler(this.ckEdge_CheckedChanged);
+            // 
+            // ckSSAO
+            // 
+            this.ckSSAO.AutoSize = true;
+            this.ckSSAO.Location = new System.Drawing.Point(325, 16);
+            this.ckSSAO.Name = "ckSSAO";
+            this.ckSSAO.Size = new System.Drawing.Size(57, 16);
+            this.ckSSAO.TabIndex = 3;
+            this.ckSSAO.Text = "SSAO";
+            this.ckSSAO.UseVisualStyleBackColor = true;
+            this.ckSSAO.CheckedChanged += new System.EventHandler(this.ckSSAO_CheckedChanged);
+            // 
+            // ckShadow
+            // 
+            this.ckShadow.AutoSize = true;
+            this.ckShadow.Location = new System.Drawing.Point(436, 16);
+            this.ckShadow.Name = "ckShadow";
+            this.ckShadow.Size = new System.Drawing.Size(60, 16);
+            this.ckShadow.TabIndex = 4;
+            this.ckShadow.Text = "그림자";
+            this.ckShadow.UseVisualStyleBackColor = true;
+            this.ckShadow.CheckedChanged += new System.EventHandler(this.ckShadow_CheckedChanged);
+            // 
+            // ckEnvLight
+            // 
+            this.ckEnvLight.AutoSize = true;
+            this.ckEnvLight.Location = new System.Drawing.Point(542, 16);
+            this.ckEnvLight.Name = "ckEnvLight";
+            this.ckEnvLight.Size = new System.Drawing.Size(72, 16);
+            this.ckEnvLight.TabIndex = 5;
+            this.ckEnvLight.Text = "환경조명";
+            this.ckEnvLight.UseVisualStyleBackColor = true;
+            this.ckEnvLight.CheckedChanged += new System.EventHandler(this.ckEnvLight_CheckedChanged);
+            // 
             // FrmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(921, 545);
+            this.Controls.Add(this.ckEnvLight);
+            this.Controls.Add(this.ckShadow);
+            this.Controls.Add(this.ckSSAO);
+            this.Controls.Add(this.ckEdge);
             this.Controls.Add(this.btnOpen);
             this.Controls.Add(this.splitContainer1);
             this.Name = "FrmMain";
@@ -69,6 +121,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -76,6 +129,10 @@
 
         private System.Windows.Forms.SplitContainer splitContainer1;
         private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.CheckBox ckEdge;
+        private System.Windows.Forms.CheckBox ckSSAO;
+        private System.Windows.Forms.CheckBox ckShadow;
+        private System.Windows.Forms.CheckBox ckEnvLight;
     }
 }
 
--- CoreExample/VIZCore3DX.NET.Demo/FrmMain.cs
+++ CoreExample/VIZCore3DX.NET.Demo/FrmMain.cs
@@ -31,32 +31,66 @@ namespace VIZCore3DX.NET.Demo
         {
             InitializeComponent();
 
+            // 코어
             vizcore3d = new Core();
 
+            // 뷰 컨트롤
             View = new ViewControl();
             View.Dock = DockStyle.Fill;
             View.Initialize(vizcore3d);
             this.splitContainer1.Panel2.Controls.Add(View);
 
+            // 씬 트리 컨트롤
             Scene = new SceneControl();
             Scene.Dock = DockStyle.Fill;
             Scene.Initialize(vizcore3d);
             this.splitContainer1.Panel1.Controls.Add(Scene);
-        }
 
-        private void btnOpen_Click(object sender, EventArgs e)
-        {
+            // 라이선스
             VIZCore3DX.NET.License.AuthenticationResult resultFile =
                 vizcore3d.AuthenticateLicenseByFile("C:\\License\\VIZCore3DX.NET.lic");
 
             //VIZCore3DX.NET.License.AuthenticationResult resultServer =
             //    vizcore3d.AuthenticateLicenseByServer("127.0.0.1", 8901);
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            if (vizcore3d.IsLicenseAuthenticated != License.AuthenticationResult.Success) return;
 
             OpenFileDialog dlg = new OpenFileDialog();
             dlg.Filter = "VIZX (*.vizx)|*.vizx";
             if (dlg.ShowDialog() != DialogResult.OK) return;
 
             vizcore3d.OpenFile(dlg.FileName);
+        }
+
+        private void ckEdge_CheckedChanged(object sender, EventArgs e)
+        {
+            if (vizcore3d.IsLicenseAuthenticated != License.AuthenticationResult.Success) return;
+
+            vizcore3d.View.IsEdgeEnabled = ckEdge.Checked;
+        }
+
+        private void ckSSAO_CheckedChanged(object sender, EventArgs e)
+        {
+            if (vizcore3d.IsLicenseAuthenticated != License.AuthenticationResult.Success) return;
+
+            vizcore3d.View.IsSsaoEnabled = ckSSAO.Checked;
+        }
+
+        private void ckShadow_CheckedChanged(object sender, EventArgs e)
+        {
+            if (vizcore3d.IsLicenseAuthenticated != License.AuthenticationResult.Success) return;
+
+            vizcore3d.View.IsShadowEnabled = ckShadow.Checked;
+        }
+
+        private void ckEnvLight_CheckedChanged(object sender, EventArgs e)
+        {
+            if (vizcore3d.IsLicenseAuthenticated != License.AuthenticationResult.Success) return;
+
+            vizcore3d.View.IsEnvironmentMapEnabled = ckEnvLight.Checked;
         }
     }
 }
