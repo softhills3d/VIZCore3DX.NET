6abeecdec9b19f1980a679f7034352a66ffe1f7d
Gwangjung, Kim <gjkim@softhills.net>
2024-05-23 07:52:07 +0900

    Add Menu

--- CoreExample/VIZCore3DX.NET.Basic/FrmMain.cs
+++ CoreExample/VIZCore3DX.NET.Basic/FrmMain.cs
@@ -46,14 +46,21 @@ namespace VIZCore3DX.NET.Basic
             Scene.Initialize(vizcore3d);
             this.splitContainerMain.Panel1.Controls.Add(Scene);
 
-            // 라이선스
-            //VIZCore3DX.NET.License.AuthenticationResult resultFile =
+            // 라이선스: File
+            //VIZCore3DX.NET.License.AuthenticationResult result =
             //    vizcore3d.AuthenticateLicenseByFile("C:\\License\\VIZCore3DX.NET.lic");
 
-            VIZCore3DX.NET.License.AuthenticationResult resultServer =
+            // 라이선스: Server
+            VIZCore3DX.NET.License.AuthenticationResult result =
                 vizcore3d.AuthenticateLicenseByServer("127.0.0.1", 8901);
+
+            if (result != VIZCore3DX.NET.License.AuthenticationResult.Success)
+            {
+                MessageBox.Show(string.Format("Error Code = {0}", result), "License", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
+        #region 메뉴 - 파일
         private void menuFileNew_Click(object sender, EventArgs e)
         {
             vizcore3d.CloseFile();
@@ -84,5 +91,91 @@ namespace VIZCore3DX.NET.Basic
 
             vizcore3d.View.StartRender();
         }
+
+        private void menuFileExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        #endregion
+
+        #region 메뉴 - 편집
+        private void menuEditUndo_Click(object sender, EventArgs e)
+        {
+            vizcore3d.Undo();
+        }
+
+        private void menuEditRedo_Click(object sender, EventArgs e)
+        {
+            vizcore3d.Redo();
+        }
+        #endregion
+
+        #region 메뉴 - 보기
+        private void menuViewRenderingModeSmooth_Click(object sender, EventArgs e)
+        {
+            vizcore3d.View.IsEdgeEnabled = false;
+        }
+
+        private void menuViewRenderingModeSmoothEdge_Click(object sender, EventArgs e)
+        {
+            vizcore3d.View.IsEdgeEnabled = true;
+        }
+
+        private void menuViewRenderingModeSSAO_Click(object sender, EventArgs e)
+        {
+            vizcore3d.View.IsSsaoEnabled = !vizcore3d.View.IsSsaoEnabled;
+        }
+
+        private void menuViewRenderingModeRealtimeShadow_Click(object sender, EventArgs e)
+        {
+            vizcore3d.View.IsShadowEnabled = !vizcore3d.View.IsShadowEnabled;
+        }
+
+        private void menuViewRenderingModeEnvironmentLight_Click(object sender, EventArgs e)
+        {
+            vizcore3d.View.IsEnvironmentMapEnabled = !vizcore3d.View.IsEnvironmentMapEnabled;
+        }
+
+        private void menuViewXray_Click(object sender, EventArgs e)
+        {
+            vizcore3d.View.IsXRayEnabled = !vizcore3d.View.IsXRayEnabled;
+        }
+        #endregion
+
+        #region 메뉴 - 도구
+        private void menuToolsLicenseFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "License (*.lic)|*.lic";
+            if (dlg.ShowDialog() != DialogResult.OK) return;
+
+            VIZCore3DX.NET.License.AuthenticationResult result =
+                vizcore3d.AuthenticateLicenseByFile(dlg.FileName);
+
+            if (result != VIZCore3DX.NET.License.AuthenticationResult.Success)
+            {
+                MessageBox.Show(string.Format("Error Code = {0}", result), "License", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                MessageBox.Show("License = Success", "License", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void menuToolsLicenseServer_Click(object sender, EventArgs e)
+        {
+            VIZCore3DX.NET.License.AuthenticationResult result =
+                vizcore3d.AuthenticateLicenseByServer("127.0.0.1", 8901);
+
+            if (result != VIZCore3DX.NET.License.AuthenticationResult.Success)
+            {
+                MessageBox.Show(string.Format("Error Code = {0}", result), "License", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                MessageBox.Show("License = Success", "License", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        #endregion
     }
 }
