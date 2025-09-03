768296e87602ae6ed458560d5fffdf0a78848a76
sclee <sclee@softhills.net>
2025-09-03 12:42:27 +0900

    Search 예제 추가

---
+++ WinForms/VIZCore3DX.NET.Search/Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace VIZCore3DX.NET.Search
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSearch1 = new System.Windows.Forms.Button();
+            this.txtKeyword1 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lvResult1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnSearch2 = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.lvResult2 = new System.Windows.Forms.ListView();
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.tabControl1);
+            this.splitContainer1.Size = new System.Drawing.Size(1008, 729);
+            this.splitContainer1.SplitterDistance = 243;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.btnSearch1);
+            this.groupBox1.Controls.Add(this.txtKeyword1);
+            this.groupBox1.Location = new System.Drawing.Point(6, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(223, 100);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Search";
+            // 
+            // btnSearch1
+            // 
+            this.btnSearch1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSearch1.Location = new System.Drawing.Point(128, 64);
+            this.btnSearch1.Name = "btnSearch1";
+            this.btnSearch1.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch1.TabIndex = 1;
+            this.btnSearch1.Text = "Search";
+            this.btnSearch1.UseVisualStyleBackColor = true;
+            this.btnSearch1.Click += new System.EventHandler(this.btnSearch1_Click);
+            // 
+            // txtKeyword1
+            // 
+            this.txtKeyword1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtKeyword1.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtKeyword1.Location = new System.Drawing.Point(23, 32);
+            this.txtKeyword1.Name = "txtKeyword1";
+            this.txtKeyword1.Size = new System.Drawing.Size(180, 21);
+            this.txtKeyword1.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.lvResult1);
+            this.groupBox2.Location = new System.Drawing.Point(5, 112);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(223, 585);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Result";
+            // 
+            // lvResult1
+            // 
+            this.lvResult1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.lvResult1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lvResult1.FullRowSelect = true;
+            this.lvResult1.GridLines = true;
+            this.lvResult1.HideSelection = false;
+            this.lvResult1.Location = new System.Drawing.Point(3, 17);
+            this.lvResult1.Name = "lvResult1";
+            this.lvResult1.Size = new System.Drawing.Size(217, 565);
+            this.lvResult1.TabIndex = 0;
+            this.lvResult1.UseCompatibleStateImageBehavior = false;
+            this.lvResult1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "IDX";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "ID";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Name";
+            this.columnHeader3.Width = 90;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(243, 729);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.groupBox1);
+            this.tabPage1.Controls.Add(this.groupBox2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(235, 703);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Search";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.groupBox4);
+            this.tabPage2.Controls.Add(this.groupBox3);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(235, 703);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Searchs";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox3.Controls.Add(this.btnSearch2);
+            this.groupBox3.Location = new System.Drawing.Point(6, 6);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(223, 65);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Search";
+            // 
+            // btnSearch2
+            // 
+            this.btnSearch2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSearch2.Location = new System.Drawing.Point(142, 25);
+            this.btnSearch2.Name = "btnSearch2";
+            this.btnSearch2.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch2.TabIndex = 1;
+            this.btnSearch2.Text = "Search";
+            this.btnSearch2.UseVisualStyleBackColor = true;
+            this.btnSearch2.Click += new System.EventHandler(this.btnSearch2_Click);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox4.Controls.Add(this.lvResult2);
+            this.groupBox4.Location = new System.Drawing.Point(6, 77);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(223, 620);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Result";
+            // 
+            // lvResult2
+            // 
+            this.lvResult2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6});
+            this.lvResult2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lvResult2.FullRowSelect = true;
+            this.lvResult2.GridLines = true;
+            this.lvResult2.HideSelection = false;
+            this.lvResult2.Location = new System.Drawing.Point(3, 17);
+            this.lvResult2.Name = "lvResult2";
+            this.lvResult2.Size = new System.Drawing.Size(217, 600);
+            this.lvResult2.TabIndex = 0;
+            this.lvResult2.UseCompatibleStateImageBehavior = false;
+            this.lvResult2.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "IDX";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "ID";
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Name";
+            this.columnHeader6.Width = 90;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 729);
+            this.Controls.Add(this.splitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "VIZCore3DX.NET.Search";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnSearch1;
+        private System.Windows.Forms.TextBox txtKeyword1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListView lvResult1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnSearch2;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.ListView lvResult2;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+    }
+}
+
---
+++ WinForms/VIZCore3DX.NET.Search/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VIZCore3DX.NET.Search
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// VIZCore3DX.NET Control
+        /// </summary>
+        public VIZCore3DX.NET.VIZCore3DXControl vizcore3dx { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // VIZCore3DX Module Init
+            VIZCore3DX.NET.ModuleInitializer.Run();
+            vizcore3dx = new VIZCore3DXControl();
+            vizcore3dx.Dock = DockStyle.Fill;
+            vizcore3dx.OnInitializedVIZCore3DX += VIZCore3D_OnInitializedVIZCore3D;
+
+            // Panel Control Add
+            splitContainer1.Panel2.Controls.Add(vizcore3dx);
+        }
+
+        private void VIZCore3D_OnInitializedVIZCore3D(object sender, EventArgs e)
+        {
+            // 라이선스 서버를 통한 인증
+            vizcore3dx.License.LicenseServer("192.168.0.215", 8901);
+
+            InitializeVIZCore3DXEvent();
+        }
+
+        /// <summary>
+        /// VIZCore3DX.NET 전역 이벤트 초기화
+        /// </summary>
+        private void InitializeVIZCore3DXEvent()
+        {
+            
+        }
+
+        private void btnSearch1_Click(object sender, EventArgs e)
+        {
+            vizcore3dx.ShowWaitForm("Searching...", "Searching...");
+
+            List<string> keyword = new List<string>();
+            keyword.Add(txtKeyword1.Text.Trim());
+
+            List<Data.Node> nodes = vizcore3dx.Object3D.Find.QuickSearch
+            (keyword
+              , false /* Join Condition : True - And, False - Or */
+              , false /* True : Assembly Only, False : Assembly + Part */
+              , true /* True : Visible Only, False : All Node */
+              , false /* True : Selected Object, False : All Node */
+              , false /* True : Full Match, False : Contains */
+            );
+
+            vizcore3dx.Object3D.Select(nodes, true);
+
+            lvResult1.Items.Clear();
+
+            lvResult1.BeginUpdate();
+            foreach (VIZCore3DX.NET.Data.Node item in nodes)
+            {
+                ListViewItem lvi = new ListViewItem(new string[] { item.Index.ToString(), item.EntityID.ToString(), item.NodeName });
+                lvResult1.Items.Add(lvi);
+            }
+            lvResult1.EndUpdate();
+
+            vizcore3dx.CloseWaitForm();
+        }
+
+        private void btnSearch2_Click(object sender, EventArgs e)
+        {
+            vizcore3dx.ShowWaitForm("Searching...", "Searching...");
+
+            List<string> keyword = new List<string>();
+            keyword.Add("*OBST*");
+            keyword.Add("*INSU*");
+
+            List<Data.Node> nodes = vizcore3dx.Object3D.Find.QuickSearch
+            (keyword
+              , false /* Join Condition : True - And, False - Or */
+              , false /* True : Assembly Only, False : Assembly + Part */
+              , true /* True : Visible Only, False : All Node */
+              , false /* True : Selected Object, False : All Node */
+              , false /* True : Full Match, False : Contains */
+            );
+
+            vizcore3dx.Object3D.Select(nodes, true);
+
+            lvResult2.Items.Clear();
+
+            lvResult2.BeginUpdate();
+            foreach (VIZCore3DX.NET.Data.Node item in nodes)
+            {
+                ListViewItem lvi = new ListViewItem(new string[] { item.Index.ToString(), item.EntityID.ToString(), item.NodeName });
+                lvResult2.Items.Add(lvi);
+            }
+            lvResult2.EndUpdate();
+
+            vizcore3dx.CloseWaitForm();
+        }
+    }
+}
