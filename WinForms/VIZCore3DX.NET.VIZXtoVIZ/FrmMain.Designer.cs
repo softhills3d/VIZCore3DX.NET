c66682b5431078da490bf1e2f4a31eae8ebb6073
sbyoon <sbyoon@softhills.net>
2025-11-24 15:54:55 +0900

    VIZX to VIZ 변환 예제 추가

---
+++ WinForms/VIZCore3DX.NET.VIZXtoVIZ/FrmMain.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace VIZCore3DX.NET.VIZXtoVIZ
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cbViewCmd = new System.Windows.Forms.CheckBox();
+            this.cbSnapshotCheck = new System.Windows.Forms.CheckBox();
+            this.btnSetConvertPath = new System.Windows.Forms.Button();
+            this.convertPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnOpenModel = new System.Windows.Forms.Button();
+            this.btnAddModels = new System.Windows.Forms.Button();
+            this.btnCloseModel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox3);
+            this.splitContainer1.Size = new System.Drawing.Size(1130, 687);
+            this.splitContainer1.SplitterDistance = 376;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cbViewCmd);
+            this.groupBox1.Controls.Add(this.cbSnapshotCheck);
+            this.groupBox1.Controls.Add(this.btnSetConvertPath);
+            this.groupBox1.Controls.Add(this.convertPath);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnConvert);
+            this.groupBox1.Location = new System.Drawing.Point(12, 113);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(355, 562);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "VIZX to VIZ";
+            // 
+            // cbViewCmd
+            // 
+            this.cbViewCmd.AutoSize = true;
+            this.cbViewCmd.Location = new System.Drawing.Point(174, 118);
+            this.cbViewCmd.Name = "cbViewCmd";
+            this.cbViewCmd.Size = new System.Drawing.Size(147, 16);
+            this.cbViewCmd.TabIndex = 8;
+            this.cbViewCmd.Text = "Check CMD Progress";
+            this.cbViewCmd.UseVisualStyleBackColor = true;
+            // 
+            // cbSnapshotCheck
+            // 
+            this.cbSnapshotCheck.AutoSize = true;
+            this.cbSnapshotCheck.Location = new System.Drawing.Point(15, 118);
+            this.cbSnapshotCheck.Name = "cbSnapshotCheck";
+            this.cbSnapshotCheck.Size = new System.Drawing.Size(123, 16);
+            this.cbSnapshotCheck.TabIndex = 7;
+            this.cbSnapshotCheck.Text = "Split by Snapshot";
+            this.cbSnapshotCheck.UseVisualStyleBackColor = true;
+            // 
+            // btnSetConvertPath
+            // 
+            this.btnSetConvertPath.Location = new System.Drawing.Point(15, 29);
+            this.btnSetConvertPath.Name = "btnSetConvertPath";
+            this.btnSetConvertPath.Size = new System.Drawing.Size(157, 23);
+            this.btnSetConvertPath.TabIndex = 3;
+            this.btnSetConvertPath.Text = "Set Convert Path";
+            this.btnSetConvertPath.UseVisualStyleBackColor = true;
+            this.btnSetConvertPath.Click += new System.EventHandler(this.btnSetConvertPath_Click);
+            // 
+            // convertPath
+            // 
+            this.convertPath.Location = new System.Drawing.Point(123, 69);
+            this.convertPath.Name = "convertPath";
+            this.convertPath.ReadOnly = true;
+            this.convertPath.Size = new System.Drawing.Size(220, 21);
+            this.convertPath.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Convert File Path:";
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(15, 140);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(157, 23);
+            this.btnConvert.TabIndex = 4;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnOpenModel);
+            this.groupBox3.Controls.Add(this.btnAddModels);
+            this.groupBox3.Controls.Add(this.btnCloseModel);
+            this.groupBox3.Location = new System.Drawing.Point(12, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(355, 95);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Model";
+            // 
+            // btnOpenModel
+            // 
+            this.btnOpenModel.Location = new System.Drawing.Point(15, 38);
+            this.btnOpenModel.Name = "btnOpenModel";
+            this.btnOpenModel.Size = new System.Drawing.Size(101, 23);
+            this.btnOpenModel.TabIndex = 0;
+            this.btnOpenModel.Text = "Open";
+            this.btnOpenModel.UseVisualStyleBackColor = true;
+            this.btnOpenModel.Click += new System.EventHandler(this.btnOpenModel_Click);
+            // 
+            // btnAddModels
+            // 
+            this.btnAddModels.Location = new System.Drawing.Point(126, 38);
+            this.btnAddModels.Name = "btnAddModels";
+            this.btnAddModels.Size = new System.Drawing.Size(101, 23);
+            this.btnAddModels.TabIndex = 1;
+            this.btnAddModels.Text = "Add";
+            this.btnAddModels.UseVisualStyleBackColor = true;
+            this.btnAddModels.Click += new System.EventHandler(this.btnAddModels_Click);
+            // 
+            // btnCloseModel
+            // 
+            this.btnCloseModel.Location = new System.Drawing.Point(239, 38);
+            this.btnCloseModel.Name = "btnCloseModel";
+            this.btnCloseModel.Size = new System.Drawing.Size(101, 23);
+            this.btnCloseModel.TabIndex = 2;
+            this.btnCloseModel.Text = "Close";
+            this.btnCloseModel.UseVisualStyleBackColor = true;
+            this.btnCloseModel.Click += new System.EventHandler(this.btnCloseModel_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1130, 687);
+            this.Controls.Add(this.splitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "VIZCore3DX.NET.VIZXtoVIZ";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnOpenModel;
+        private System.Windows.Forms.Button btnAddModels;
+        private System.Windows.Forms.Button btnCloseModel;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.TextBox convertPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSetConvertPath;
+        private System.Windows.Forms.CheckBox cbSnapshotCheck;
+        private System.Windows.Forms.CheckBox cbViewCmd;
+    }
+}
+
---
+++ WinForms/VIZCore3DX.NET.VIZXtoVIZ/FrmMain.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Windows.Forms;
+using VIZCore3DX.NET.Data;
+using VIZCore3DX.NET.Event;
+using VIZCore3DX.NET.Manager;
+using static VIZCore3DX.NET.Manager.SectionManager;
+
+namespace VIZCore3DX.NET.VIZXtoVIZ
+{
+    public partial class FrmMain : Form
+    {
+        public VIZCore3DX.NET.VIZCore3DXControl vizcore3dx { get; set; }
+
+        public FrmMain()
+        {
+            InitializeComponent();
+
+            VIZCore3DX.NET.ModuleInitializer.Run();
+
+            vizcore3dx = new VIZCore3DXControl();
+            vizcore3dx.Dock = DockStyle.Fill;
+            splitContainer1.Panel2.Controls.Add(vizcore3dx);
+
+            vizcore3dx.OnInitializedVIZCore3DX += VIZCore3D_OnInitializedVIZCore3D;
+        }
+
+        private void VIZCore3D_OnInitializedVIZCore3D(object sender, EventArgs e)
+        {
+            // ================================================================
+            // Example
+            // ================================================================
+            // 라이선스 파일을 통한 인증
+            //vizcore3dx.License.LicenseFile("C:\\Temp\\VIZCore3DX.NET.lic");
+
+            // 라이선스 서버를 통한 인증
+            VIZCore3DX.NET.Data.LicenseResults result = vizcore3dx.License.LicenseServer("192.168.100.252", 8901);
+
+
+            // ================================================================
+            // License
+            // ================================================================
+            // VIZCore3DX.NET.Data.LicenseResults result = vizcore3dx.License.LicenseFile("C:\\License\\VIZCore3DX.NET.lic");
+            if (result != VIZCore3DX.NET.Data.LicenseResults.SUCCESS)
+            {
+                MessageBox.Show(string.Format("LICENSE CODE : {0}", result.ToString()), "VIZCore3DX.NET", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            InitializeVIZCore3DX();
+        }
+
+        private void InitializeVIZCore3DX()
+        {
+            // ================================================================
+            // 모델 열기 시, 3D 화면 Rendering 차단
+            // ================================================================
+            vizcore3dx.BeginUpdate();
+
+            // ================================================================
+            // 설정 - 툴바
+            // ================================================================
+            vizcore3dx.ToolbarMain.Visible = true;
+            vizcore3dx.ToolbarNote.Visible = false;
+            vizcore3dx.ToolbarMeasure.Visible = false;
+            vizcore3dx.ToolbarSection.Visible = false;
+            vizcore3dx.ToolbarSnapshot.Visible = false;
+
+            // ================================================================
+            // 모델 열기 시, 3D 화면 Rendering 재시작
+            // ================================================================
+            vizcore3dx.EndUpdate();
+        }
+
+        /// <summary>
+        /// VIZX to VIZ 변환
+        /// </summary>
+        /// <param name="inputFilePath">변환 전 vizx 파일</param>
+        /// <param name="outputFilePath">변환 후 저장할 viz 파일</param>
+        /// <param name="exeFileName">VIZX to VIZ 변환기 프로그램</param>
+        /// <param name="argument">스냅샷 분할 여부를 체크하는 argument</param>
+        private void convertVizxToViz(string inputFilePath, string outputFilePath, string exeFileName, string argument)
+        {
+            if (!System.IO.File.Exists(inputFilePath))
+            {
+                MessageBox.Show($"File not found : \n{inputFilePath}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            string currentPath = Application.StartupPath;
+            string convertFilePath = Path.Combine(currentPath, exeFileName);
+
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+
+            /*
+             * VIZX to VIZ 사용법
+             * 중요!!! : 같은 디렉토리 내에 VIZXMigration.exe 파일과 ShdCore.dll 파일이 존재해야함.
+             *
+             * 1. 일반적인 사용 방법
+             * VIZXMigration.exe -mode VIZX2VIZ -i "변환 전 vizx 파일경로" -o "변환 후 viz 파일 경로"
+             *
+             * 2. 스냅샷 별로 파일 분할이 필요한 경우
+             * VIZXMigration.exe -mode VIZX2VIZ -i "변환 전 vizx 파일경로" -o "변환 후 viz 파일 경로" -export_snapshot_to_viz t
+            */
+
+
+            // CMD 창을 띄우지 않고 백그라운드에서 실행
+            if (!cbViewCmd.Checked)
+            {
+                startInfo.FileName = exeFileName;
+                startInfo.Arguments = $"-mode VIZX2VIZ -i \"{inputFilePath}\" -o \"{outputFilePath}\" {argument}";
+                startInfo.CreateNoWindow = true;
+                startInfo.UseShellExecute = false;
+            }
+            else
+            {
+                startInfo.FileName = "cmd.exe";
+                startInfo.Arguments = $"/k {exeFileName} -mode VIZX2VIZ -i \"{inputFilePath}\" -o \"{outputFilePath}\" {argument}";
+            }
+
+                try
+                {
+                    bool status = vizcore3dx.EnableWaitForm;
+                    Process p = Process.Start(startInfo);
+
+                    vizcore3dx.EnableWaitForm = false;
+                    vizcore3dx.ShowWaitForm();
+                    vizcore3dx.UpdateWaitForm("Please Wait...", "Processing...");
+
+                    // VIZX to VIZ 변환기가 종료될 때 까지 Wait
+                    p.WaitForExit();
+
+                    vizcore3dx.CloseWaitForm();
+                    vizcore3dx.EnableWaitForm = status;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error : {ex.Message}");
+                }
+        }
+
+        private void btnOpenModel_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = vizcore3dx.Model.OpenFilter;
+            if (dlg.ShowDialog() != DialogResult.OK) return;
+
+            vizcore3dx.Model.Open(dlg.FileName);
+        }
+
+        private void btnAddModels_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = vizcore3dx.Model.OpenFilter;
+            dlg.Multiselect = true;
+            if (dlg.ShowDialog() != DialogResult.OK) return;
+
+            vizcore3dx.Model.Add(dlg.FileNames);
+        }
+
+        private void btnCloseModel_Click(object sender, EventArgs e)
+        {
+            if (vizcore3dx.Model.IsOpen() == false) return;
+
+            vizcore3dx.Model.Close();
+        }
+
+        /// <summary>
+        ///  viz 변환된 파일이 저장 될 경로 지정
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSetConvertPath_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDlg = new FolderBrowserDialog();
+            folderDlg.Description = "Please Set Convert Path.";
+
+            if (folderDlg.ShowDialog() == DialogResult.OK)
+            {
+                convertPath.Text = folderDlg.SelectedPath;
+            }
+        }
+
+        /// <summary>
+        /// VIZX to VIZ 변환기 실행
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            if (vizcore3dx.Model.IsOpen() == false) return;
+            if (convertPath.Text == "") return;
+
+            DialogResult result = MessageBox.Show("The current file is saved.", "Caution", MessageBoxButtons.OKCancel,MessageBoxIcon.Information);
+            if (result == DialogResult.Cancel) return;
+
+            string outputFileName = "output.viz";
+            string inputFilePath;
+            string outputFilePath = Path.Combine(convertPath.Text, outputFileName);
+            string convertExe = "VIZXMigration.exe";
+            string argument = "";
+
+            vizcore3dx.Model.SaveFileDialog();
+            inputFilePath = vizcore3dx.Model.Files[0];
+
+            // 스냅샷 별로 분할하여 viz 파일들을 저장하는 args : -export_snapshot_to_viz t
+            if (cbSnapshotCheck.Checked)
+            {
+                argument = "-export_snapshot_to_viz t";
+            }
+
+            convertVizxToViz(inputFilePath, outputFilePath, convertExe, argument);
+        }
+
+    }
+}
