523e75d8feca90bc63e16786424b460f3a8a3b00
sbyoon <sbyoon@softhills.net>
2025-12-17 15:34:40 +0900

    SelectByBox 로 노드 선택 시 ASSEMBLY 노드 정보 포함 예제 추가

---
+++ WinForms/VIZCore3DX.NET.SelectByBox_IncludeAssembly/FrmMain.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace VIZCore3DX.NET.SelectByBox_IncludeAssembly
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnOpenModel = new System.Windows.Forms.Button();
+            this.btnAddModels = new System.Windows.Forms.Button();
+            this.btnCloseModel = new System.Windows.Forms.Button();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.resultGrid = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSelectByBox = new System.Windows.Forms.Button();
+            this.btnIncludeAssembly = new System.Windows.Forms.Button();
+            this.btnSelectedAll = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.resultGrid)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox3);
+            this.splitContainer1.Size = new System.Drawing.Size(1130, 534);
+            this.splitContainer1.SplitterDistance = 376;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnOpenModel);
+            this.groupBox3.Controls.Add(this.btnAddModels);
+            this.groupBox3.Controls.Add(this.btnCloseModel);
+            this.groupBox3.Location = new System.Drawing.Point(12, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(355, 95);
+            this.groupBox3.TabIndex = 6;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Model";
+            // 
+            // btnOpenModel
+            // 
+            this.btnOpenModel.Location = new System.Drawing.Point(15, 38);
+            this.btnOpenModel.Name = "btnOpenModel";
+            this.btnOpenModel.Size = new System.Drawing.Size(101, 23);
+            this.btnOpenModel.TabIndex = 0;
+            this.btnOpenModel.Text = "Open";
+            this.btnOpenModel.UseVisualStyleBackColor = true;
+            this.btnOpenModel.Click += new System.EventHandler(this.btnOpenModel_Click);
+            // 
+            // btnAddModels
+            // 
+            this.btnAddModels.Location = new System.Drawing.Point(126, 38);
+            this.btnAddModels.Name = "btnAddModels";
+            this.btnAddModels.Size = new System.Drawing.Size(101, 23);
+            this.btnAddModels.TabIndex = 1;
+            this.btnAddModels.Text = "Add";
+            this.btnAddModels.UseVisualStyleBackColor = true;
+            this.btnAddModels.Click += new System.EventHandler(this.btnAddModels_Click);
+            // 
+            // btnCloseModel
+            // 
+            this.btnCloseModel.Location = new System.Drawing.Point(239, 38);
+            this.btnCloseModel.Name = "btnCloseModel";
+            this.btnCloseModel.Size = new System.Drawing.Size(101, 23);
+            this.btnCloseModel.TabIndex = 2;
+            this.btnCloseModel.Text = "Close";
+            this.btnCloseModel.UseVisualStyleBackColor = true;
+            this.btnCloseModel.Click += new System.EventHandler(this.btnCloseModel_Click);
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.splitContainer1);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.resultGrid);
+            this.splitContainer2.Size = new System.Drawing.Size(1130, 687);
+            this.splitContainer2.SplitterDistance = 534;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // resultGrid
+            // 
+            this.resultGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.resultGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.resultGrid.Location = new System.Drawing.Point(0, 0);
+            this.resultGrid.Name = "resultGrid";
+            this.resultGrid.RowTemplate.Height = 23;
+            this.resultGrid.Size = new System.Drawing.Size(1130, 149);
+            this.resultGrid.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnSelectedAll);
+            this.groupBox1.Controls.Add(this.btnIncludeAssembly);
+            this.groupBox1.Controls.Add(this.btnSelectByBox);
+            this.groupBox1.Location = new System.Drawing.Point(12, 113);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(355, 111);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Function";
+            // 
+            // btnSelectByBox
+            // 
+            this.btnSelectByBox.Location = new System.Drawing.Point(15, 29);
+            this.btnSelectByBox.Name = "btnSelectByBox";
+            this.btnSelectByBox.Size = new System.Drawing.Size(325, 23);
+            this.btnSelectByBox.TabIndex = 0;
+            this.btnSelectByBox.Text = "Select By Box";
+            this.btnSelectByBox.UseVisualStyleBackColor = true;
+            this.btnSelectByBox.Click += new System.EventHandler(this.btnSelectByBox_Click);
+            // 
+            // btnIncludeAssembly
+            // 
+            this.btnIncludeAssembly.Location = new System.Drawing.Point(183, 69);
+            this.btnIncludeAssembly.Name = "btnIncludeAssembly";
+            this.btnIncludeAssembly.Size = new System.Drawing.Size(157, 23);
+            this.btnIncludeAssembly.TabIndex = 1;
+            this.btnIncludeAssembly.Text = "Include Assembly";
+            this.btnIncludeAssembly.UseVisualStyleBackColor = true;
+            this.btnIncludeAssembly.Click += new System.EventHandler(this.btnIncludeAssembly_Click);
+            // 
+            // btnSelectedAll
+            // 
+            this.btnSelectedAll.Location = new System.Drawing.Point(15, 69);
+            this.btnSelectedAll.Name = "btnSelectedAll";
+            this.btnSelectedAll.Size = new System.Drawing.Size(157, 23);
+            this.btnSelectedAll.TabIndex = 2;
+            this.btnSelectedAll.Text = "Selected All";
+            this.btnSelectedAll.UseVisualStyleBackColor = true;
+            this.btnSelectedAll.Click += new System.EventHandler(this.btnSelectedAll_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1130, 687);
+            this.Controls.Add(this.splitContainer2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "VIZCore3DX.NET.SelectByBox_IncludeAssembly";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.resultGrid)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnOpenModel;
+        private System.Windows.Forms.Button btnAddModels;
+        private System.Windows.Forms.Button btnCloseModel;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnIncludeAssembly;
+        private System.Windows.Forms.Button btnSelectByBox;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.DataGridView resultGrid;
+        private System.Windows.Forms.Button btnSelectedAll;
+    }
+}
+
---
+++ WinForms/VIZCore3DX.NET.SelectByBox_IncludeAssembly/FrmMain.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VIZCore3DX.NET.Data;
+
+namespace VIZCore3DX.NET.SelectByBox_IncludeAssembly
+{
+    public partial class FrmMain : Form
+    {
+        public VIZCore3DX.NET.VIZCore3DXControl vizcore3dx { get; set; }
+
+        public FrmMain()
+        {
+            InitializeComponent();
+
+            VIZCore3DX.NET.ModuleInitializer.Run();
+
+            vizcore3dx = new VIZCore3DXControl();
+            vizcore3dx.Dock = DockStyle.Fill;
+
+            vizcore3dx.OnInitializedVIZCore3DX += VIZCore3D_OnInitializedVIZCore3D;
+
+            splitContainer1.Panel2.Controls.Add(vizcore3dx);
+        }
+
+        private void VIZCore3D_OnInitializedVIZCore3D(object sender, EventArgs e)
+        {
+            // ================================================================
+            // Example
+            // ================================================================
+            // 라이선스 파일을 통한 인증
+            //vizcore3dx.License.LicenseFile("C:\\Temp\\VIZCore3DX.NET.lic");
+
+            // 라이선스 서버를 통한 인증
+            VIZCore3DX.NET.Data.LicenseResults result = vizcore3dx.License.LicenseServer("192.168.100.252", 8901);
+
+
+            // ================================================================
+            // License
+            // ================================================================
+            // VIZCore3DX.NET.Data.LicenseResults result = vizcore3dx.License.LicenseFile("C:\\License\\VIZCore3DX.NET.lic");
+            if (result != VIZCore3DX.NET.Data.LicenseResults.SUCCESS)
+            {
+                MessageBox.Show(string.Format("LICENSE CODE : {0}", result.ToString()), "VIZCore3DX.NET", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            InitializeVIZCore3DX();
+        }
+
+        private void InitializeVIZCore3DX()
+        {
+            // ================================================================
+            // 모델 열기 시, 3D 화면 Rendering 차단
+            // ================================================================
+            vizcore3dx.BeginUpdate();
+
+            // ================================================================
+            // 설정 - 툴바
+            // ================================================================
+            vizcore3dx.ToolbarMain.Visible = true;
+            vizcore3dx.ToolbarNote.Visible = false;
+            vizcore3dx.ToolbarMeasure.Visible = false;
+            vizcore3dx.ToolbarSection.Visible = false;
+            vizcore3dx.ToolbarSnapshot.Visible = false;
+
+            // ================================================================
+            // 모델 열기 시, 3D 화면 Rendering 재시작
+            // ================================================================
+            vizcore3dx.EndUpdate();
+        }
+
+        private void btnOpenModel_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = vizcore3dx.Model.OpenFilter;
+            if (dlg.ShowDialog() != DialogResult.OK) return;
+
+            vizcore3dx.Model.Open(dlg.FileName);
+        }
+
+        private void btnAddModels_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = vizcore3dx.Model.OpenFilter;
+            dlg.Multiselect = true;
+            if (dlg.ShowDialog() != DialogResult.OK) return;
+
+            vizcore3dx.Model.Add(dlg.FileNames);
+        }
+
+        private void btnCloseModel_Click(object sender, EventArgs e)
+        {
+            if (vizcore3dx.Model.IsOpen() == false) return;
+
+            vizcore3dx.Model.Close();
+        }
+        private void btnSelectByBox_Click(object sender, EventArgs e)
+        {
+            vizcore3dx.Object3D.SelectByBox(VIZCore3DX.NET.Data.Object3DSelectionBoxModes.MULTI);
+        }
+        private void btnSelectedAll_Click(object sender, EventArgs e)
+        {
+            List<VIZCore3DX.NET.Data.Node> selectedNode = vizcore3dx.Object3D.FromFilter(VIZCore3DX.NET.Data.Object3dFilter.SELECTED_ALL);
+
+            resultGrid.DataSource = selectedNode;
+            resultGrid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            MessageBox.Show("조회된 노드 수 : " + selectedNode.Count + "개");
+        }
+
+        private void btnIncludeAssembly_Click(object sender, EventArgs e)
+        {
+            List<VIZCore3DX.NET.Data.Node> selectedNode = vizcore3dx.Object3D.FromFilter(VIZCore3DX.NET.Data.Object3dFilter.SELECTED_ALL); // selectByBox로 선택한 PART 노드
+            List<VIZCore3DX.NET.Data.Node> fullNode = vizcore3dx.Object3D.FromFilter(VIZCore3DX.NET.Data.Object3dFilter.ALL); // 모델의 전체 노드
+
+            var mapFull = fullNode.ToDictionary(n => n.Index); // 전체 노드의 Index Dictionary
+
+            var resultList = new List<Node>(selectedNode); // 결과 리스트
+            var resultSet = new HashSet<int>(selectedNode.Select(x => x.Index)); // 중복 방지용 HashSet
+
+            var queue = new Queue<Node>(); // 상위 탐색 큐
+            var childCounts = new Dictionary<int, int>(); // 자식노드 개수
+
+            foreach (var node in selectedNode) // [Step 1] 선택된 노드(PART)들의 부모 노드 찾기
+            {
+                // 딕셔너리에서 바로 Index로 부모 조회
+                if (mapFull.TryGetValue(node.ParentIndex, out Node parent))
+                {
+                    if (resultSet.Add(parent.Index))
+                    {
+                        resultList.Add(parent);
+                        queue.Enqueue(parent);
+                    }
+                }
+            }
+
+            while (queue.Count > 0) // [Step 2] 모든 자식이 있는 경우 상위 노드(Assembly) 찾기
+            {
+                var child = queue.Dequeue();
+
+                if (mapFull.TryGetValue(child.ParentIndex, out Node parent))
+                {
+                    if (!childCounts.ContainsKey(parent.Index))
+                        childCounts[parent.Index] = 0;
+
+                    childCounts[parent.Index]++;
+
+                    if (childCounts[parent.Index] == parent.ChildCount)
+                    {
+                        if (resultSet.Add(parent.Index))
+                        {
+                            resultList.Add(parent);
+                            queue.Enqueue(parent);
+                        }
+                    }
+                }
+            }
+
+            resultList.Sort((a, b) => a.Index.CompareTo(b.Index)); // Index 기준으로 리스트 정렬
+
+            resultGrid.DataSource = resultList;
+            resultGrid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            MessageBox.Show("조회된 노드 수 : " + resultList.Count + "개");
+        }
+    }
+}
