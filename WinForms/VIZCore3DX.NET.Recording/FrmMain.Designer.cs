fa78cb86520645c983d691bbcfa6a374a273327c
BaeSooHyeong <shbae@softhills.net>
2026-01-16 09:17:17 +0900

    화면 녹화 기능 예제 추가

---
+++ WinForms/VIZCore3DX.NET.Recording/FrmMain.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace VIZCore3DX.NET.Recording
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.cbCustom = new System.Windows.Forms.CheckBox();
+            this.tbOutputPath = new System.Windows.Forms.TextBox();
+            this.tbFfmpegPath = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbShowCursor = new System.Windows.Forms.CheckBox();
+            this.btnRecord = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnOpenModel = new System.Windows.Forms.Button();
+            this.btnAddModels = new System.Windows.Forms.Button();
+            this.btnCloseModel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox3);
+            this.splitContainer1.Size = new System.Drawing.Size(1130, 687);
+            this.splitContainer1.SplitterDistance = 376;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnStop);
+            this.groupBox1.Controls.Add(this.cbCustom);
+            this.groupBox1.Controls.Add(this.tbOutputPath);
+            this.groupBox1.Controls.Add(this.tbFfmpegPath);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.cbShowCursor);
+            this.groupBox1.Controls.Add(this.btnRecord);
+            this.groupBox1.Location = new System.Drawing.Point(12, 122);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(355, 277);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Recording";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(191, 220);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(146, 23);
+            this.btnStop.TabIndex = 7;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // cbCustom
+            // 
+            this.cbCustom.AutoSize = true;
+            this.cbCustom.Checked = true;
+            this.cbCustom.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbCustom.Location = new System.Drawing.Point(16, 30);
+            this.cbCustom.Name = "cbCustom";
+            this.cbCustom.Size = new System.Drawing.Size(104, 16);
+            this.cbCustom.TabIndex = 6;
+            this.cbCustom.Text = "경로 미리 지정";
+            this.cbCustom.UseVisualStyleBackColor = true;
+            this.cbCustom.CheckedChanged += new System.EventHandler(this.cbCustom_CheckedChanged);
+            // 
+            // tbOutputPath
+            // 
+            this.tbOutputPath.Location = new System.Drawing.Point(15, 140);
+            this.tbOutputPath.Name = "tbOutputPath";
+            this.tbOutputPath.Size = new System.Drawing.Size(322, 21);
+            this.tbOutputPath.TabIndex = 5;
+            this.tbOutputPath.Text = "D:\\Temp\\output.mp4";
+            // 
+            // tbFfmpegPath
+            // 
+            this.tbFfmpegPath.Location = new System.Drawing.Point(16, 76);
+            this.tbFfmpegPath.Name = "tbFfmpegPath";
+            this.tbFfmpegPath.Size = new System.Drawing.Size(322, 21);
+            this.tbFfmpegPath.TabIndex = 4;
+            this.tbFfmpegPath.Text = "C:\\ffmpeg\\bin\\ffmpeg.exe";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "2. Output 파일 경로 :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "1. ffmpeg.exe 경로 :";
+            // 
+            // cbShowCursor
+            // 
+            this.cbShowCursor.AutoSize = true;
+            this.cbShowCursor.Location = new System.Drawing.Point(34, 183);
+            this.cbShowCursor.Name = "cbShowCursor";
+            this.cbShowCursor.Size = new System.Drawing.Size(98, 16);
+            this.cbShowCursor.TabIndex = 1;
+            this.cbShowCursor.Text = "Show Cursor";
+            this.cbShowCursor.UseVisualStyleBackColor = true;
+            // 
+            // btnRecord
+            // 
+            this.btnRecord.Location = new System.Drawing.Point(16, 220);
+            this.btnRecord.Name = "btnRecord";
+            this.btnRecord.Size = new System.Drawing.Size(146, 23);
+            this.btnRecord.TabIndex = 0;
+            this.btnRecord.Text = "Start";
+            this.btnRecord.UseVisualStyleBackColor = true;
+            this.btnRecord.Click += new System.EventHandler(this.btnRecord_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnOpenModel);
+            this.groupBox3.Controls.Add(this.btnAddModels);
+            this.groupBox3.Controls.Add(this.btnCloseModel);
+            this.groupBox3.Location = new System.Drawing.Point(12, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(355, 95);
+            this.groupBox3.TabIndex = 6;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Model";
+            // 
+            // btnOpenModel
+            // 
+            this.btnOpenModel.Location = new System.Drawing.Point(15, 38);
+            this.btnOpenModel.Name = "btnOpenModel";
+            this.btnOpenModel.Size = new System.Drawing.Size(101, 23);
+            this.btnOpenModel.TabIndex = 0;
+            this.btnOpenModel.Text = "Open";
+            this.btnOpenModel.UseVisualStyleBackColor = true;
+            this.btnOpenModel.Click += new System.EventHandler(this.btnOpenModel_Click);
+            // 
+            // btnAddModels
+            // 
+            this.btnAddModels.Location = new System.Drawing.Point(126, 38);
+            this.btnAddModels.Name = "btnAddModels";
+            this.btnAddModels.Size = new System.Drawing.Size(101, 23);
+            this.btnAddModels.TabIndex = 1;
+            this.btnAddModels.Text = "Add";
+            this.btnAddModels.UseVisualStyleBackColor = true;
+            this.btnAddModels.Click += new System.EventHandler(this.btnAddModels_Click);
+            // 
+            // btnCloseModel
+            // 
+            this.btnCloseModel.Location = new System.Drawing.Point(239, 38);
+            this.btnCloseModel.Name = "btnCloseModel";
+            this.btnCloseModel.Size = new System.Drawing.Size(101, 23);
+            this.btnCloseModel.TabIndex = 2;
+            this.btnCloseModel.Text = "Close";
+            this.btnCloseModel.UseVisualStyleBackColor = true;
+            this.btnCloseModel.Click += new System.EventHandler(this.btnCloseModel_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1130, 687);
+            this.Controls.Add(this.splitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "VIZCore3DX.NET.Recording";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnOpenModel;
+        private System.Windows.Forms.Button btnAddModels;
+        private System.Windows.Forms.Button btnCloseModel;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnRecord;
+        private System.Windows.Forms.CheckBox cbShowCursor;
+        private System.Windows.Forms.TextBox tbOutputPath;
+        private System.Windows.Forms.TextBox tbFfmpegPath;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox cbCustom;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ WinForms/VIZCore3DX.NET.Recording/FrmMain.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VIZCore3DX.NET.Recording
+{
+    public partial class FrmMain : Form
+    {
+        public VIZCore3DX.NET.VIZCore3DXControl vizcore3dx { get; set; }
+
+        public FrmMain()
+        {
+            InitializeComponent();
+            btnStop.Enabled = false;
+
+            VIZCore3DX.NET.ModuleInitializer.Run();
+
+            vizcore3dx = new VIZCore3DXControl();
+            vizcore3dx.Dock = DockStyle.Fill;
+
+            vizcore3dx.OnInitializedVIZCore3DX += VIZCore3D_OnInitializedVIZCore3D;
+
+            splitContainer1.Panel2.Controls.Add(vizcore3dx);
+        }
+
+        private void VIZCore3D_OnInitializedVIZCore3D(object sender, EventArgs e)
+        {
+            // ================================================================
+            // Example
+            // ================================================================
+            // 라이선스 파일을 통한 인증
+            //vizcore3dx.License.LicenseFile("C:\\Temp\\VIZCore3DX.NET.lic");
+
+            // 라이선스 서버를 통한 인증
+            VIZCore3DX.NET.Data.LicenseResults result = vizcore3dx.License.LicenseServer("192.168.100.252", 8901);
+
+
+            // ================================================================
+            // License
+            // ================================================================
+            // VIZCore3DX.NET.Data.LicenseResults result = vizcore3dx.License.LicenseFile("C:\\License\\VIZCore3DX.NET.lic");
+            if (result != VIZCore3DX.NET.Data.LicenseResults.SUCCESS)
+            {
+                MessageBox.Show(string.Format("LICENSE CODE : {0}", result.ToString()), "VIZCore3DX.NET", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            InitializeVIZCore3DX();
+        }
+
+        private void InitializeVIZCore3DX()
+        {
+            // ================================================================
+            // 모델 열기 시, 3D 화면 Rendering 차단
+            // ================================================================
+            vizcore3dx.BeginUpdate();
+
+            // ================================================================
+            // 설정 - 툴바
+            // ================================================================
+            vizcore3dx.ToolbarMain.Visible = true;
+            vizcore3dx.ToolbarNote.Visible = false;
+            vizcore3dx.ToolbarMeasure.Visible = false;
+            vizcore3dx.ToolbarSection.Visible = false;
+            vizcore3dx.ToolbarSnapshot.Visible = false;
+
+            // ================================================================
+            // 모델 열기 시, 3D 화면 Rendering 재시작
+            // ================================================================
+            vizcore3dx.EndUpdate();
+        }
+
+        private void btnOpenModel_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = vizcore3dx.Model.OpenFilter;
+            if (dlg.ShowDialog() != DialogResult.OK) return;
+
+            vizcore3dx.Model.Open(dlg.FileName);
+        }
+
+        private void btnAddModels_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = vizcore3dx.Model.OpenFilter;
+            dlg.Multiselect = true;
+            if (dlg.ShowDialog() != DialogResult.OK) return;
+
+            vizcore3dx.Model.Add(dlg.FileNames);
+        }
+
+        private void btnCloseModel_Click(object sender, EventArgs e)
+        {
+            if (vizcore3dx.Model.IsOpen() == false) return;
+
+            vizcore3dx.Model.Close();
+        }
+        
+        private void btnRecord_Click(object sender, EventArgs e)
+        {
+            bool recordFlag;
+
+            string FfmpegPath = tbFfmpegPath.Text;
+            string OutputPath = tbOutputPath.Text;
+
+            bool chkShowCursor = cbShowCursor.Checked;
+
+            if (cbCustom.Checked == true)
+            {
+                recordFlag = vizcore3dx.StartRecording(chkShowCursor, FfmpegPath, OutputPath);
+            }
+            else
+            {
+                recordFlag = vizcore3dx.StartRecording(chkShowCursor);
+            }
+
+            if (recordFlag)
+            {
+                btnRecord.Enabled = false;
+                btnStop.Enabled = true;
+            }
+            
+        }
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            bool recordFlag = vizcore3dx.StopRecording();
+
+            if (recordFlag)
+            {
+                btnRecord.Enabled = true;
+                btnStop.Enabled = false;
+            }
+        }
+
+        private void cbCustom_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbCustom.Checked == true)
+            {
+                tbFfmpegPath.Enabled = true;
+                tbOutputPath.Enabled = true;
+            }
+            else
+            {
+                tbFfmpegPath.Enabled = false;
+                tbOutputPath.Enabled = false;
+            }
+        }
+
+        
+    }
+}
