ae4174d8185ba6789103efde9ac510bf1f334c87
gjkim <gjkim@softhills.net>
2026-04-13 21:55:28 +0900

    Split Objects

---
+++ WinForms/VIZCore3DX.NET.SplitObjects/FrmMain.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace VIZCore3DX.NET.SplitObjects
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSplit = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ptX = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ptY = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ptZ = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.vZ = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.vY = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.vX = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox2);
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(1177, 694);
+            this.splitContainer1.SplitterDistance = 249;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.vZ);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.vY);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.vX);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.ptZ);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.ptY);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.ptX);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(228, 244);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Plane";
+            // 
+            // btnSplit
+            // 
+            this.btnSplit.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSplit.Location = new System.Drawing.Point(31, 29);
+            this.btnSplit.Name = "btnSplit";
+            this.btnSplit.Size = new System.Drawing.Size(159, 23);
+            this.btnSplit.TabIndex = 0;
+            this.btnSplit.Text = "Split";
+            this.btnSplit.UseVisualStyleBackColor = true;
+            this.btnSplit.Click += new System.EventHandler(this.btnSplit_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.btnSplit);
+            this.groupBox2.Location = new System.Drawing.Point(12, 264);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(228, 77);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Split Objects";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Point";
+            // 
+            // ptX
+            // 
+            this.ptX.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ptX.Location = new System.Drawing.Point(66, 45);
+            this.ptX.Name = "ptX";
+            this.ptX.Size = new System.Drawing.Size(142, 21);
+            this.ptX.TabIndex = 1;
+            this.ptX.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "X";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(42, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Y";
+            // 
+            // ptY
+            // 
+            this.ptY.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ptY.Location = new System.Drawing.Point(66, 72);
+            this.ptY.Name = "ptY";
+            this.ptY.Size = new System.Drawing.Size(142, 21);
+            this.ptY.TabIndex = 3;
+            this.ptY.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(42, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Z";
+            // 
+            // ptZ
+            // 
+            this.ptZ.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ptZ.Location = new System.Drawing.Point(66, 99);
+            this.ptZ.Name = "ptZ";
+            this.ptZ.Size = new System.Drawing.Size(142, 21);
+            this.ptZ.TabIndex = 5;
+            this.ptZ.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(42, 211);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 12);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Z";
+            // 
+            // vZ
+            // 
+            this.vZ.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.vZ.Location = new System.Drawing.Point(66, 208);
+            this.vZ.Name = "vZ";
+            this.vZ.Size = new System.Drawing.Size(142, 21);
+            this.vZ.TabIndex = 12;
+            this.vZ.Text = "1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(42, 184);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 12);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Y";
+            // 
+            // vY
+            // 
+            this.vY.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.vY.Location = new System.Drawing.Point(66, 181);
+            this.vY.Name = "vY";
+            this.vY.Size = new System.Drawing.Size(142, 21);
+            this.vY.TabIndex = 10;
+            this.vY.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(42, 157);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(13, 12);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "X";
+            // 
+            // vX
+            // 
+            this.vX.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.vX.Location = new System.Drawing.Point(66, 154);
+            this.vX.Name = "vX";
+            this.vX.Size = new System.Drawing.Size(142, 21);
+            this.vX.TabIndex = 8;
+            this.vX.Text = "0";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(22, 138);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(41, 12);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Vector";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1177, 694);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "VIZCore3DX.NET.SplitObjects";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnSplit;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox vZ;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox vY;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox vX;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox ptZ;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox ptY;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox ptX;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WinForms/VIZCore3DX.NET.SplitObjects/FrmMain.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VIZCore3DX.NET.SplitObjects
+{
+    public partial class FrmMain : Form
+    {
+        private VIZCore3DX.NET.VIZCore3DXControl vizcore3dx { get; set; }
+
+        public FrmMain()
+        {
+            InitializeComponent();
+
+            VIZCore3DX.NET.ModuleInitializer.Run();
+
+            vizcore3dx = new VIZCore3DX.NET.VIZCore3DXControl();
+            vizcore3dx.Dock = DockStyle.Fill;
+            vizcore3dx.OnInitializedVIZCore3DX += VIZCore3DX_OnInitializedVIZCore3DX;
+
+            splitContainer1.Panel2.Controls.Add(vizcore3dx);
+        }
+
+        private void VIZCore3DX_OnInitializedVIZCore3DX(object sender, EventArgs e)
+        {
+            VIZCore3DX.NET.Data.LicenseResults result
+                = vizcore3dx.License.LicenseServer("127.0.0.1", 8901);
+
+            if (result != VIZCore3DX.NET.Data.LicenseResults.SUCCESS) return;
+        }
+
+        private void btnSplit_Click(object sender, EventArgs e)
+        {
+            List<VIZCore3DX.NET.Data.Node> selectedPart =
+                    vizcore3dx.Object3D.FromFilter(VIZCore3DX.NET.Data.Object3dFilter.SELECTED_ALL);
+
+            if (selectedPart.Count == 0)
+            {
+                MessageBox.Show("Selected Items is empty.");
+                return;
+            }
+
+            VIZCore3DX.NET.Data.Plane3D plane = new VIZCore3DX.NET.Data.Plane3D(
+                new VIZCore3DX.NET.Data.Vector3D(
+                    Convert.ToSingle(ptX.Text)
+                    , Convert.ToSingle(ptY.Text)
+                    , Convert.ToSingle(ptZ.Text)
+                    ),
+
+                new VIZCore3DX.NET.Data.Vector3D(
+                    Convert.ToSingle(vX.Text)
+                    , Convert.ToSingle(vY.Text)
+                    , Convert.ToSingle(vZ.Text)
+                    )
+                );
+
+            vizcore3dx.Object3D.SplitMesh(selectedPart, plane);
+        }
+    }
+}
