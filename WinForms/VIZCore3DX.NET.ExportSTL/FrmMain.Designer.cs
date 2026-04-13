a03598826a29febfe096598000441e0d3b5d771f
gjkim <gjkim@softhills.net>
2026-04-13 23:05:39 +0900

    STL 내보내기

---
+++ WinForms/VIZCore3DX.NET.ExportSTL/FrmMain.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace VIZCore3DX.NET.ExportSTL
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnExportStlAscii = new System.Windows.Forms.Button();
+            this.btnExportStlBinary = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(984, 605);
+            this.splitContainer1.SplitterDistance = 248;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.btnExportStlBinary);
+            this.groupBox1.Controls.Add(this.btnExportStlAscii);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(227, 112);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Export";
+            // 
+            // btnExportStlAscii
+            // 
+            this.btnExportStlAscii.Location = new System.Drawing.Point(29, 36);
+            this.btnExportStlAscii.Name = "btnExportStlAscii";
+            this.btnExportStlAscii.Size = new System.Drawing.Size(160, 23);
+            this.btnExportStlAscii.TabIndex = 0;
+            this.btnExportStlAscii.Text = "STL (ASCII)";
+            this.btnExportStlAscii.UseVisualStyleBackColor = true;
+            this.btnExportStlAscii.Click += new System.EventHandler(this.btnExportStlAscii_Click);
+            // 
+            // btnExportStlBinary
+            // 
+            this.btnExportStlBinary.Location = new System.Drawing.Point(29, 65);
+            this.btnExportStlBinary.Name = "btnExportStlBinary";
+            this.btnExportStlBinary.Size = new System.Drawing.Size(160, 23);
+            this.btnExportStlBinary.TabIndex = 1;
+            this.btnExportStlBinary.Text = "STL (BINARY)";
+            this.btnExportStlBinary.UseVisualStyleBackColor = true;
+            this.btnExportStlBinary.Click += new System.EventHandler(this.btnExportStlBinary_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 605);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Export STL";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnExportStlBinary;
+        private System.Windows.Forms.Button btnExportStlAscii;
+    }
+}
+
---
+++ WinForms/VIZCore3DX.NET.ExportSTL/FrmMain.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VIZCore3DX.NET.ExportSTL
+{
+    public partial class FrmMain : Form
+    {
+        private VIZCore3DX.NET.VIZCore3DXControl vizcore3dx { get; set; }
+
+        public FrmMain()
+        {
+            InitializeComponent();
+
+            VIZCore3DX.NET.ModuleInitializer.Run();
+
+            vizcore3dx = new VIZCore3DX.NET.VIZCore3DXControl();
+            vizcore3dx.Dock = DockStyle.Fill;
+            vizcore3dx.OnInitializedVIZCore3DX += VIZCore3DX_OnInitializedVIZCore3DX;
+
+            splitContainer1.Panel2.Controls.Add(vizcore3dx);
+        }
+
+        private void VIZCore3DX_OnInitializedVIZCore3DX(object sender, EventArgs e)
+        {
+            VIZCore3DX.NET.Data.LicenseResults result
+                = vizcore3dx.License.LicenseServer("127.0.0.1", 8901);
+
+            if (result != VIZCore3DX.NET.Data.LicenseResults.SUCCESS) return;
+        }
+
+        private void btnExportStlAscii_Click(object sender, EventArgs e)
+        {
+            if (vizcore3dx.Model.IsOpen() == false) return;
+
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.Filter = "STL File (*.stl)|*.stl";
+            if (dlg.ShowDialog() != DialogResult.OK) return;
+
+            vizcore3dx.Model.ExportStl(dlg.FileName, true);
+        }
+
+        private void btnExportStlBinary_Click(object sender, EventArgs e)
+        {
+            if (vizcore3dx.Model.IsOpen() == false) return;
+
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.Filter = "STL File (*.stl)|*.stl";
+            if (dlg.ShowDialog() != DialogResult.OK) return;
+
+            vizcore3dx.Model.ExportStl(dlg.FileName, false);
+        }
+    }
+}
