79ce4ef29989e2580989b145dfbf09c3b8c07979
BaeSooHyeong <shbae@softhills.net>
2026-04-27 15:59:52 +0900

    Render Scale 예제 추가

---
+++ WinForms/VIZCore3DX.NET.RenderScale/FrmMain.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace VIZCore3DX.NET.RenderScale
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnOpenModel = new System.Windows.Forms.Button();
+            this.btnAddModels = new System.Windows.Forms.Button();
+            this.btnCloseModel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox3);
+            this.splitContainer1.Size = new System.Drawing.Size(1130, 687);
+            this.splitContainer1.SplitterDistance = 376;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.trackBar1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 129);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(355, 96);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "RenderScale";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.LargeChange = 100;
+            this.trackBar1.Location = new System.Drawing.Point(6, 20);
+            this.trackBar1.Maximum = 400;
+            this.trackBar1.Minimum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(343, 45);
+            this.trackBar1.SmallChange = 50;
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.TickFrequency = 50;
+            this.trackBar1.Value = 100;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnOpenModel);
+            this.groupBox3.Controls.Add(this.btnAddModels);
+            this.groupBox3.Controls.Add(this.btnCloseModel);
+            this.groupBox3.Location = new System.Drawing.Point(12, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(355, 95);
+            this.groupBox3.TabIndex = 6;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Model";
+            // 
+            // btnOpenModel
+            // 
+            this.btnOpenModel.Location = new System.Drawing.Point(15, 38);
+            this.btnOpenModel.Name = "btnOpenModel";
+            this.btnOpenModel.Size = new System.Drawing.Size(101, 23);
+            this.btnOpenModel.TabIndex = 0;
+            this.btnOpenModel.Text = "Open";
+            this.btnOpenModel.UseVisualStyleBackColor = true;
+            this.btnOpenModel.Click += new System.EventHandler(this.btnOpenModel_Click);
+            // 
+            // btnAddModels
+            // 
+            this.btnAddModels.Location = new System.Drawing.Point(126, 38);
+            this.btnAddModels.Name = "btnAddModels";
+            this.btnAddModels.Size = new System.Drawing.Size(101, 23);
+            this.btnAddModels.TabIndex = 1;
+            this.btnAddModels.Text = "Add";
+            this.btnAddModels.UseVisualStyleBackColor = true;
+            this.btnAddModels.Click += new System.EventHandler(this.btnAddModels_Click);
+            // 
+            // btnCloseModel
+            // 
+            this.btnCloseModel.Location = new System.Drawing.Point(239, 38);
+            this.btnCloseModel.Name = "btnCloseModel";
+            this.btnCloseModel.Size = new System.Drawing.Size(101, 23);
+            this.btnCloseModel.TabIndex = 2;
+            this.btnCloseModel.Text = "Close";
+            this.btnCloseModel.UseVisualStyleBackColor = true;
+            this.btnCloseModel.Click += new System.EventHandler(this.btnCloseModel_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(255, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1130, 687);
+            this.Controls.Add(this.splitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "VIZCore3DX.NET.RenderScale";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnOpenModel;
+        private System.Windows.Forms.Button btnAddModels;
+        private System.Windows.Forms.Button btnCloseModel;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WinForms/VIZCore3DX.NET.RenderScale/FrmMain.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace VIZCore3DX.NET.RenderScale
+{
+    public partial class FrmMain : Form
+    {
+        public VIZCore3DX.NET.VIZCore3DXControl vizcore3dx { get; set; }
+
+        public FrmMain()
+        {
+            InitializeComponent();
+
+            VIZCore3DX.NET.ModuleInitializer.Run();
+
+            vizcore3dx = new VIZCore3DXControl();
+            vizcore3dx.Dock = DockStyle.Fill;
+
+            vizcore3dx.OnInitializedVIZCore3DX += VIZCore3D_OnInitializedVIZCore3D;
+
+            splitContainer1.Panel2.Controls.Add(vizcore3dx);
+        }
+
+        private void VIZCore3D_OnInitializedVIZCore3D(object sender, EventArgs e)
+        {
+            // ================================================================
+            // Example
+            // ================================================================
+            // 라이선스 파일을 통한 인증
+            //vizcore3dx.License.LicenseFile("C:\\Temp\\VIZCore3DX.NET.lic");
+
+            // 라이선스 서버를 통한 인증
+            VIZCore3DX.NET.Data.LicenseResults result = vizcore3dx.License.LicenseServer("192.168.100.252", 8901);
+
+
+            // ================================================================
+            // License
+            // ================================================================
+            // VIZCore3DX.NET.Data.LicenseResults result = vizcore3dx.License.LicenseFile("C:\\License\\VIZCore3DX.NET.lic");
+            if (result != VIZCore3DX.NET.Data.LicenseResults.SUCCESS)
+            {
+                MessageBox.Show(string.Format("LICENSE CODE : {0}", result.ToString()), "VIZCore3DX.NET", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            InitializeVIZCore3DX();
+            InitializeVIZCore3DXEvent();
+        }
+
+        private void InitializeVIZCore3DX()
+        {
+            // ================================================================
+            // 모델 열기 시, 3D 화면 Rendering 차단
+            // ================================================================
+            vizcore3dx.BeginUpdate();
+
+            // ================================================================
+            // 설정 - 툴바
+            // ================================================================
+            vizcore3dx.ToolbarMain.Visible = true;
+            vizcore3dx.ToolbarNote.Visible = false;
+            vizcore3dx.ToolbarMeasure.Visible = false;
+            vizcore3dx.ToolbarSection.Visible = false;
+            vizcore3dx.ToolbarSnapshot.Visible = false;
+            vizcore3dx.ToolbarDecal.Visible = false;
+
+            // ================================================================
+            // 모델 열기 시, 3D 화면 Rendering 재시작
+            // ================================================================
+            vizcore3dx.EndUpdate();
+
+            // 렌더 스케일 지정 및 업데이트
+            UpdateFloatValue();
+        }
+        private void InitializeVIZCore3DXEvent()
+        {
+        }
+
+        private void btnOpenModel_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = vizcore3dx.Model.OpenFilter;
+            if (dlg.ShowDialog() != DialogResult.OK) return;
+
+            vizcore3dx.Model.Open(dlg.FileName);
+        }
+
+        private void btnAddModels_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = vizcore3dx.Model.OpenFilter;
+            dlg.Multiselect = true;
+            if (dlg.ShowDialog() != DialogResult.OK) return;
+
+            vizcore3dx.Model.Add(dlg.FileNames);
+        }
+
+        private void btnCloseModel_Click(object sender, EventArgs e)
+        {
+            if (vizcore3dx.Model.IsOpen() == false) return;
+
+            vizcore3dx.Model.Close();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            UpdateFloatValue();
+        }
+
+        private void UpdateFloatValue()
+        {
+            // 정수 Value를 100.0f로 나누어 다시 소수점으로 변환
+            float floatValue = trackBar1.Value / 100.0f;
+
+            // 라벨에 출력하여 확인 (소수점 둘째 자리까지 표시)
+            label1.Text = $"현재 값: {floatValue:F2}";
+
+            
+            vizcore3dx.View.RenderScale = floatValue;
+        }
+    }
+}
