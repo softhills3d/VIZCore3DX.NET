76c5a6f192329e8e4d826dafeaf295163ac5a99e
BaeSooHyeong <shbae@softhills.net>
2026-05-19 11:06:11 +0900

    Add ToolbarHide Example

---
+++ WinForms/VIZCore3DX.NET.ToolbarHide/FrmMain.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace VIZCore3DX.NET.ToolbarHide
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cbToolbarList = new System.Windows.Forms.ComboBox();
+            this.btnToolbarShow = new System.Windows.Forms.Button();
+            this.btnToolbarHide = new System.Windows.Forms.Button();
+            this.lvToolbar = new System.Windows.Forms.ListView();
+            this.btnGetToolbarList = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.btnOpenModel = new System.Windows.Forms.Button();
+            this.btnAddModels = new System.Windows.Forms.Button();
+            this.btnCloseModel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox3);
+            this.splitContainer1.Size = new System.Drawing.Size(1130, 687);
+            this.splitContainer1.SplitterDistance = 376;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cbToolbarList);
+            this.groupBox1.Controls.Add(this.btnToolbarShow);
+            this.groupBox1.Controls.Add(this.btnToolbarHide);
+            this.groupBox1.Controls.Add(this.lvToolbar);
+            this.groupBox1.Controls.Add(this.btnGetToolbarList);
+            this.groupBox1.Location = new System.Drawing.Point(12, 113);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(355, 494);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Toolbar Setting";
+            // 
+            // cbToolbarList
+            // 
+            this.cbToolbarList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbToolbarList.FormattingEnabled = true;
+            this.cbToolbarList.Location = new System.Drawing.Point(15, 35);
+            this.cbToolbarList.Name = "cbToolbarList";
+            this.cbToolbarList.Size = new System.Drawing.Size(325, 20);
+            this.cbToolbarList.TabIndex = 5;
+            // 
+            // btnToolbarShow
+            // 
+            this.btnToolbarShow.Location = new System.Drawing.Point(15, 437);
+            this.btnToolbarShow.Name = "btnToolbarShow";
+            this.btnToolbarShow.Size = new System.Drawing.Size(149, 39);
+            this.btnToolbarShow.TabIndex = 4;
+            this.btnToolbarShow.Text = "Show Selected";
+            this.btnToolbarShow.UseVisualStyleBackColor = true;
+            this.btnToolbarShow.Click += new System.EventHandler(this.btnToolbarShow_Click);
+            // 
+            // btnToolbarHide
+            // 
+            this.btnToolbarHide.Location = new System.Drawing.Point(191, 437);
+            this.btnToolbarHide.Name = "btnToolbarHide";
+            this.btnToolbarHide.Size = new System.Drawing.Size(149, 39);
+            this.btnToolbarHide.TabIndex = 2;
+            this.btnToolbarHide.Text = "Hide Selected";
+            this.btnToolbarHide.UseVisualStyleBackColor = true;
+            this.btnToolbarHide.Click += new System.EventHandler(this.btnToolbarHide_Click);
+            // 
+            // lvToolbar
+            // 
+            this.lvToolbar.HideSelection = false;
+            this.lvToolbar.Location = new System.Drawing.Point(15, 123);
+            this.lvToolbar.Name = "lvToolbar";
+            this.lvToolbar.Size = new System.Drawing.Size(325, 300);
+            this.lvToolbar.TabIndex = 1;
+            this.lvToolbar.UseCompatibleStateImageBehavior = false;
+            // 
+            // btnGetToolbarList
+            // 
+            this.btnGetToolbarList.Location = new System.Drawing.Point(15, 72);
+            this.btnGetToolbarList.Name = "btnGetToolbarList";
+            this.btnGetToolbarList.Size = new System.Drawing.Size(325, 33);
+            this.btnGetToolbarList.TabIndex = 0;
+            this.btnGetToolbarList.Text = "Get Toolbar List";
+            this.btnGetToolbarList.UseVisualStyleBackColor = true;
+            this.btnGetToolbarList.Click += new System.EventHandler(this.btnGetToolbarList_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnOpenModel);
+            this.groupBox3.Controls.Add(this.btnAddModels);
+            this.groupBox3.Controls.Add(this.btnCloseModel);
+            this.groupBox3.Location = new System.Drawing.Point(12, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(355, 95);
+            this.groupBox3.TabIndex = 7;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Model";
+            // 
+            // btnOpenModel
+            // 
+            this.btnOpenModel.Location = new System.Drawing.Point(15, 38);
+            this.btnOpenModel.Name = "btnOpenModel";
+            this.btnOpenModel.Size = new System.Drawing.Size(101, 23);
+            this.btnOpenModel.TabIndex = 0;
+            this.btnOpenModel.Text = "Open";
+            this.btnOpenModel.UseVisualStyleBackColor = true;
+            this.btnOpenModel.Click += new System.EventHandler(this.btnOpenModel_Click);
+            // 
+            // btnAddModels
+            // 
+            this.btnAddModels.Location = new System.Drawing.Point(126, 38);
+            this.btnAddModels.Name = "btnAddModels";
+            this.btnAddModels.Size = new System.Drawing.Size(101, 23);
+            this.btnAddModels.TabIndex = 1;
+            this.btnAddModels.Text = "Add";
+            this.btnAddModels.UseVisualStyleBackColor = true;
+            this.btnAddModels.Click += new System.EventHandler(this.btnAddModels_Click);
+            // 
+            // btnCloseModel
+            // 
+            this.btnCloseModel.Location = new System.Drawing.Point(239, 38);
+            this.btnCloseModel.Name = "btnCloseModel";
+            this.btnCloseModel.Size = new System.Drawing.Size(101, 23);
+            this.btnCloseModel.TabIndex = 2;
+            this.btnCloseModel.Text = "Close";
+            this.btnCloseModel.UseVisualStyleBackColor = true;
+            this.btnCloseModel.Click += new System.EventHandler(this.btnCloseModel_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1130, 687);
+            this.Controls.Add(this.splitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "VIZCore3DX.NET.ToolbarHide";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button btnOpenModel;
+        private System.Windows.Forms.Button btnAddModels;
+        private System.Windows.Forms.Button btnCloseModel;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnGetToolbarList;
+        private System.Windows.Forms.ListView lvToolbar;
+        private System.Windows.Forms.Button btnToolbarShow;
+        private System.Windows.Forms.Button btnToolbarHide;
+        private System.Windows.Forms.ComboBox cbToolbarList;
+    }
+}
+
---
+++ WinForms/VIZCore3DX.NET.ToolbarHide/FrmMain.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using VIZCore3DX.NET.Data;
+using VIZCore3DX.NET.Event;
+using VIZCore3DX.NET.Manager;
+using static VIZCore3DX.NET.Manager.SectionManager;
+
+namespace VIZCore3DX.NET.ToolbarHide
+{
+    public partial class FrmMain : Form
+    {
+        public VIZCore3DX.NET.VIZCore3DXControl vizcore3dx { get; set; }
+
+        public VIZCore3DX.NET.Data.SectionItem Section { get; set; }
+
+        private bool isScroll = false;
+
+        public FrmMain()
+        {
+            InitializeComponent();
+
+            VIZCore3DX.NET.ModuleInitializer.Run();
+
+            vizcore3dx = new VIZCore3DXControl();
+            vizcore3dx.Dock = DockStyle.Fill;
+
+            vizcore3dx.OnInitializedVIZCore3DX += VIZCore3D_OnInitializedVIZCore3D;
+
+            splitContainer1.Panel2.Controls.Add(vizcore3dx);
+        }
+
+        private void VIZCore3D_OnInitializedVIZCore3D(object sender, EventArgs e)
+        {
+            // ================================================================
+            // Example
+            // ================================================================
+            // 라이선스 파일을 통한 인증
+            //vizcore3dx.License.LicenseFile("C:\\Temp\\VIZCore3DX.NET.lic");
+
+            // 라이선스 서버를 통한 인증
+            //vizcore3dx.License.LicenseServer("127.0.0.1", 8901);
+
+            // ================================================================
+            // License
+            // ================================================================
+            VIZCore3DX.NET.Data.LicenseResults result = vizcore3dx.License.LicenseFile("C:\\License\\VIZCore3DX.NET.lic");
+
+            if (result != VIZCore3DX.NET.Data.LicenseResults.SUCCESS)
+            {
+                MessageBox.Show(string.Format("LICENSE CODE : {0}", result.ToString()), "VIZCore3DX.NET", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            InitializeVIZCore3DX();
+            InitializeVIZCore3DXEvent();
+        }
+
+        private void InitializeVIZCore3DX()
+        {
+            // ================================================================
+            // 모델 열기 시, 3D 화면 Rendering 차단
+            // ================================================================
+            vizcore3dx.BeginUpdate();
+
+            // ================================================================
+            // 설정 - 툴바
+            // ================================================================
+            vizcore3dx.ToolbarMain.Visible = true;
+            vizcore3dx.ToolbarNote.Visible = false;
+            vizcore3dx.ToolbarMeasure.Visible = false;
+            vizcore3dx.ToolbarSection.Visible = false;
+            vizcore3dx.ToolbarSnapshot.Visible = false;
+            vizcore3dx.ToolbarDecal.Visible = false;
+            vizcore3dx.ToolbarPrimitive.Visible = false;
+
+            // ================================================================
+            // 모델 열기 시, 3D 화면 Rendering 재시작
+            // ================================================================
+            vizcore3dx.EndUpdate();
+
+            AddToolbar();
+        }
+
+        private void InitializeVIZCore3DXEvent()
+        {
+            
+        }
+
+        private void AddToolbar()
+        {
+            cbToolbarList.Items.Clear();
+            cbToolbarList.Items.Add("ToolbarMain");
+            cbToolbarList.Items.Add("ToolbarNote");
+            cbToolbarList.Items.Add("ToolbarMeasure");
+            cbToolbarList.Items.Add("ToolbarSection");
+            cbToolbarList.Items.Add("ToolbarSnapshot");
+            cbToolbarList.Items.Add("ToolbarDecal");
+            cbToolbarList.Items.Add("ToolbarPrimitive");
+
+            if (cbToolbarList.Items.Count > 0)
+                cbToolbarList.SelectedIndex = 0;
+
+            lvToolbar.View = View.Details;
+            lvToolbar.FullRowSelect = true;
+            lvToolbar.MultiSelect = true;
+            lvToolbar.Columns.Add("Name", 175);
+            lvToolbar.Columns.Add("Type", 95);
+            lvToolbar.Columns.Add("Visible", 55);
+        }
+
+        private System.Windows.Forms.ToolStrip GetToolbar(string name)
+        {
+            switch (name)
+            {
+                case "ToolbarMain":      return vizcore3dx.ToolbarMain;
+                case "ToolbarNote":      return vizcore3dx.ToolbarNote;
+                case "ToolbarMeasure":   return vizcore3dx.ToolbarMeasure;
+                case "ToolbarSection":   return vizcore3dx.ToolbarSection;
+                case "ToolbarSnapshot":  return vizcore3dx.ToolbarSnapshot;
+                case "ToolbarDecal":     return vizcore3dx.ToolbarDecal;
+                case "ToolbarPrimitive": return vizcore3dx.ToolbarPrimitive;
+                default:                 return null;
+            }
+        }
+
+        private void btnOpenModel_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = vizcore3dx.Model.OpenFilter;
+            if (dlg.ShowDialog() != DialogResult.OK) return;
+
+            vizcore3dx.Model.Open(dlg.FileName);
+        }
+
+        private void btnAddModels_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = vizcore3dx.Model.OpenFilter;
+            dlg.Multiselect = true;
+            if (dlg.ShowDialog() != DialogResult.OK) return;
+
+            vizcore3dx.Model.Add(dlg.FileNames);
+        }
+
+        private void btnCloseModel_Click(object sender, EventArgs e)
+        {
+            if (vizcore3dx.Model.IsOpen() == false) return;
+
+            vizcore3dx.Model.Close();
+        }
+
+        private void btnGetToolbarList_Click(object sender, EventArgs e)
+        {
+            lvToolbar.Items.Clear();
+
+            string selected = cbToolbarList.SelectedItem as string;
+            if (string.IsNullOrEmpty(selected)) return;
+
+            System.Windows.Forms.ToolStrip toolbar = GetToolbar(selected);
+            if (toolbar == null) return;
+
+            foreach (ToolStripItem item in toolbar.Items)
+            {
+                ListViewItem lvi = new ListViewItem(item.Name);
+                lvi.SubItems.Add(item.GetType().Name);
+                lvi.SubItems.Add(item.Visible.ToString());
+                lvi.Tag = Tuple.Create<string, string>(null, item.Name);
+                lvToolbar.Items.Add(lvi);
+
+                ToolStripDropDownButton dropDown = item as ToolStripDropDownButton;
+                if (dropDown != null)
+                {
+                    foreach (ToolStripItem subItem in dropDown.DropDownItems)
+                    {
+                        ListViewItem subLvi = new ListViewItem("  └ " + subItem.Name);
+                        subLvi.SubItems.Add(subItem.GetType().Name);
+                        subLvi.SubItems.Add(subItem.Visible.ToString());
+                        subLvi.Tag = Tuple.Create(dropDown.Name, subItem.Name);
+                        lvToolbar.Items.Add(subLvi);
+                    }
+                }
+            }
+        }
+
+        // Tag에 저장된 (부모명, 아이템명) 키로 ToolStripItem을 Name 기반으로 조회
+        private ToolStripItem FindToolStripItem(System.Windows.Forms.ToolStrip toolbar, Tuple<string, string> key)
+        {
+            if (key.Item1 == null)
+                return toolbar.Items[key.Item2];
+
+            ToolStripDropDownButton parent = toolbar.Items[key.Item1] as ToolStripDropDownButton;
+            if (parent == null) return null;
+
+            return parent.DropDownItems[key.Item2];
+        }
+
+        private void SetSelectedItemsVisible(bool visible)
+        {
+            string selected = cbToolbarList.SelectedItem as string;
+            if (string.IsNullOrEmpty(selected)) return;
+
+            System.Windows.Forms.ToolStrip toolbar = GetToolbar(selected);
+            if (toolbar == null) return;
+
+            foreach (ListViewItem lvi in lvToolbar.SelectedItems)
+            {
+                Tuple<string, string> key = lvi.Tag as Tuple<string, string>;
+                if (key == null) continue;
+
+                ToolStripItem item = FindToolStripItem(toolbar, key);
+                if (item == null) continue;
+
+                item.Visible = visible;
+                lvi.SubItems[2].Text = visible.ToString();
+            }
+        }
+
+        private void btnToolbarShow_Click(object sender, EventArgs e)
+        {
+            SetSelectedItemsVisible(true);
+        }
+
+        private void btnToolbarHide_Click(object sender, EventArgs e)
+        {
+            SetSelectedItemsVisible(false);
+        }
+    }
+}
