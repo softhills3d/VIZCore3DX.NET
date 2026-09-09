7bc7f4612eb7aaff181e89d6db5cbb0a7243c7cd
yjkwak <yjkwak@softhills.net>
2026-09-09 12:57:42 +0900

    Locked Select 잠금 및 표시 상태 관리 예제 추가

---
+++ WinForms/VIZCore3DX.NET.LockedSelect/FrmMain.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace VIZCore3DX.NET.LockedSelect
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.grpLockedList = new System.Windows.Forms.GroupBox();
+            this.btnHideAll = new System.Windows.Forms.Button();
+            this.btnShowAll = new System.Windows.Forms.Button();
+            this.btnHide = new System.Windows.Forms.Button();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.dgvLockedSelect = new System.Windows.Forms.DataGridView();
+            this.colNodeName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colKind = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colVisible = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.grpLockedSelect = new System.Windows.Forms.GroupBox();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.lblCountTitle = new System.Windows.Forms.Label();
+            this.btnClearLock = new System.Windows.Forms.Button();
+            this.btnUnlockSelected = new System.Windows.Forms.Button();
+            this.btnAddSelected = new System.Windows.Forms.Button();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.grpModel = new System.Windows.Forms.GroupBox();
+            this.btnOpenModel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.grpLockedList.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLockedSelect)).BeginInit();
+            this.grpLockedSelect.SuspendLayout();
+            this.grpModel.SuspendLayout();
+            this.SuspendLayout();
+
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.grpLockedList);
+            this.splitContainer1.Panel1.Controls.Add(this.grpLockedSelect);
+            this.splitContainer1.Panel1.Controls.Add(this.grpModel);
+            this.splitContainer1.Panel1.Padding = new System.Windows.Forms.Padding(8);
+            this.splitContainer1.Size = new System.Drawing.Size(1184, 661);
+            this.splitContainer1.SplitterDistance = 394;
+            this.splitContainer1.TabIndex = 0;
+
+            // 
+            // grpLockedList
+            // 
+            this.grpLockedList.Controls.Add(this.btnHideAll);
+            this.grpLockedList.Controls.Add(this.btnShowAll);
+            this.grpLockedList.Controls.Add(this.btnHide);
+            this.grpLockedList.Controls.Add(this.btnShow);
+            this.grpLockedList.Controls.Add(this.dgvLockedSelect);
+            this.grpLockedList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grpLockedList.Location = new System.Drawing.Point(8, 234);
+            this.grpLockedList.Name = "grpLockedList";
+            this.grpLockedList.Size = new System.Drawing.Size(378, 419);
+            this.grpLockedList.TabIndex = 2;
+            this.grpLockedList.TabStop = false;
+            this.grpLockedList.Text = "Locked Object List";
+
+            // 
+            // btnHideAll
+            // 
+            this.btnHideAll.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnHideAll.Location = new System.Drawing.Point(194, 378);
+            this.btnHideAll.Name = "btnHideAll";
+            this.btnHideAll.Size = new System.Drawing.Size(165, 23);
+            this.btnHideAll.TabIndex = 4;
+            this.btnHideAll.Text = "Hide All";
+            this.btnHideAll.UseVisualStyleBackColor = true;
+            this.btnHideAll.Click += new System.EventHandler(this.BtnHideAll_Click);
+
+            // 
+            // btnShowAll
+            // 
+            this.btnShowAll.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnShowAll.Location = new System.Drawing.Point(18, 378);
+            this.btnShowAll.Name = "btnShowAll";
+            this.btnShowAll.Size = new System.Drawing.Size(165, 23);
+            this.btnShowAll.TabIndex = 3;
+            this.btnShowAll.Text = "Show All";
+            this.btnShowAll.UseVisualStyleBackColor = true;
+            this.btnShowAll.Click += new System.EventHandler(this.BtnShowAll_Click);
+
+            // 
+            // btnHide
+            // 
+            this.btnHide.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnHide.Location = new System.Drawing.Point(194, 346);
+            this.btnHide.Name = "btnHide";
+            this.btnHide.Size = new System.Drawing.Size(165, 23);
+            this.btnHide.TabIndex = 2;
+            this.btnHide.Text = "Hide Selected";
+            this.btnHide.UseVisualStyleBackColor = true;
+            this.btnHide.Click += new System.EventHandler(this.BtnHide_Click);
+
+            // 
+            // btnShow
+            // 
+            this.btnShow.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnShow.Location = new System.Drawing.Point(18, 346);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(165, 23);
+            this.btnShow.TabIndex = 1;
+            this.btnShow.Text = "Show Selected";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.BtnShow_Click);
+
+            // 
+            // dgvLockedSelect
+            // 
+            this.dgvLockedSelect.AllowUserToAddRows = false;
+            this.dgvLockedSelect.AllowUserToDeleteRows = false;
+            this.dgvLockedSelect.AllowUserToResizeRows = false;
+            this.dgvLockedSelect.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvLockedSelect.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvLockedSelect.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLockedSelect.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] { this.colNodeName, this.colKind, this.colVisible });
+            this.dgvLockedSelect.Location = new System.Drawing.Point(18, 25);
+            this.dgvLockedSelect.Name = "dgvLockedSelect";
+            this.dgvLockedSelect.RowHeadersVisible = false;
+            this.dgvLockedSelect.RowTemplate.Height = 23;
+            this.dgvLockedSelect.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvLockedSelect.Size = new System.Drawing.Size(341, 302);
+            this.dgvLockedSelect.TabIndex = 0;
+            this.dgvLockedSelect.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.DgvLockedSelect_CellValueChanged);
+            this.dgvLockedSelect.CurrentCellDirtyStateChanged += new System.EventHandler(this.DgvLockedSelect_CurrentCellDirtyStateChanged);
+
+            // 
+            // colNodeName
+            // 
+            this.colNodeName.FillWeight = 180F;
+            this.colNodeName.HeaderText = "Name";
+            this.colNodeName.Name = "colNodeName";
+            this.colNodeName.ReadOnly = true;
+
+            // 
+            // colKind
+            // 
+            this.colKind.FillWeight = 90F;
+            this.colKind.HeaderText = "Kind";
+            this.colKind.Name = "colKind";
+            this.colKind.ReadOnly = true;
+
+            // 
+            // colVisible
+            // 
+            this.colVisible.FillWeight = 70F;
+            this.colVisible.HeaderText = "Visible";
+            this.colVisible.Name = "colVisible";
+
+            // 
+            // grpLockedSelect
+            // 
+            this.grpLockedSelect.Controls.Add(this.lblCount);
+            this.grpLockedSelect.Controls.Add(this.lblCountTitle);
+            this.grpLockedSelect.Controls.Add(this.btnClearLock);
+            this.grpLockedSelect.Controls.Add(this.btnUnlockSelected);
+            this.grpLockedSelect.Controls.Add(this.btnAddSelected);
+            this.grpLockedSelect.Controls.Add(this.lblDescription);
+            this.grpLockedSelect.Dock = System.Windows.Forms.DockStyle.Top;
+            this.grpLockedSelect.Location = new System.Drawing.Point(8, 78);
+            this.grpLockedSelect.Name = "grpLockedSelect";
+            this.grpLockedSelect.Size = new System.Drawing.Size(378, 156);
+            this.grpLockedSelect.TabIndex = 1;
+            this.grpLockedSelect.TabStop = false;
+            this.grpLockedSelect.Text = "Locked Select";
+
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Location = new System.Drawing.Point(68, 132);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(11, 12);
+            this.lblCount.TabIndex = 5;
+            this.lblCount.Text = "0";
+
+            // 
+            // lblCountTitle
+            // 
+            this.lblCountTitle.AutoSize = true;
+            this.lblCountTitle.Location = new System.Drawing.Point(18, 132);
+            this.lblCountTitle.Name = "lblCountTitle";
+            this.lblCountTitle.Size = new System.Drawing.Size(46, 12);
+            this.lblCountTitle.TabIndex = 4;
+            this.lblCountTitle.Text = "Count :";
+
+            // 
+            // btnClearLock
+            // 
+            this.btnClearLock.Location = new System.Drawing.Point(194, 95);
+            this.btnClearLock.Name = "btnClearLock";
+            this.btnClearLock.Size = new System.Drawing.Size(165, 23);
+            this.btnClearLock.TabIndex = 3;
+            this.btnClearLock.Text = "Clear All";
+            this.btnClearLock.UseVisualStyleBackColor = true;
+            this.btnClearLock.Click += new System.EventHandler(this.BtnClearLock_Click);
+
+            // 
+            // btnUnlockSelected
+            // 
+            this.btnUnlockSelected.Location = new System.Drawing.Point(18, 95);
+            this.btnUnlockSelected.Name = "btnUnlockSelected";
+            this.btnUnlockSelected.Size = new System.Drawing.Size(165, 23);
+            this.btnUnlockSelected.TabIndex = 2;
+            this.btnUnlockSelected.Text = "Unlock Selected";
+            this.btnUnlockSelected.UseVisualStyleBackColor = true;
+            this.btnUnlockSelected.Click += new System.EventHandler(this.BtnUnlockSelected_Click);
+
+            // 
+            // btnAddSelected
+            // 
+            this.btnAddSelected.Location = new System.Drawing.Point(18, 53);
+            this.btnAddSelected.Name = "btnAddSelected";
+            this.btnAddSelected.Size = new System.Drawing.Size(341, 32);
+            this.btnAddSelected.TabIndex = 1;
+            this.btnAddSelected.Text = "Add Selected";
+            this.btnAddSelected.UseVisualStyleBackColor = true;
+            this.btnAddSelected.Click += new System.EventHandler(this.BtnAddSelected_Click);
+
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Location = new System.Drawing.Point(18, 26);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(256, 12);
+            this.lblDescription.TabIndex = 0;
+            this.lblDescription.Text = "View에서 선택한 개체만 선택 고정 목록에 추가합니다.";
+
+            // 
+            // grpModel
+            // 
+            this.grpModel.Controls.Add(this.btnOpenModel);
+            this.grpModel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.grpModel.Location = new System.Drawing.Point(8, 8);
+            this.grpModel.Name = "grpModel";
+            this.grpModel.Size = new System.Drawing.Size(378, 70);
+            this.grpModel.TabIndex = 0;
+            this.grpModel.TabStop = false;
+            this.grpModel.Text = "Model";
+
+            // 
+            // btnOpenModel
+            // 
+            this.btnOpenModel.Location = new System.Drawing.Point(18, 24);
+            this.btnOpenModel.Name = "btnOpenModel";
+            this.btnOpenModel.Size = new System.Drawing.Size(120, 23);
+            this.btnOpenModel.TabIndex = 0;
+            this.btnOpenModel.Text = "Open Model";
+            this.btnOpenModel.UseVisualStyleBackColor = true;
+            this.btnOpenModel.Click += new System.EventHandler(this.BtnOpenModel_Click);
+
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1184, 661);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "VIZCore3DX.NET.LockedSelect";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.grpLockedList.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLockedSelect)).EndInit();
+            this.grpLockedSelect.ResumeLayout(false);
+            this.grpLockedSelect.PerformLayout();
+            this.grpModel.ResumeLayout(false);
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.GroupBox grpModel;
+        private System.Windows.Forms.Button btnOpenModel;
+        private System.Windows.Forms.GroupBox grpLockedSelect;
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.Button btnAddSelected;
+        private System.Windows.Forms.Button btnUnlockSelected;
+        private System.Windows.Forms.Button btnClearLock;
+        private System.Windows.Forms.Label lblCountTitle;
+        private System.Windows.Forms.Label lblCount;
+        private System.Windows.Forms.GroupBox grpLockedList;
+        private System.Windows.Forms.DataGridView dgvLockedSelect;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colNodeName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colKind;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn colVisible;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.Button btnHide;
+        private System.Windows.Forms.Button btnShowAll;
+        private System.Windows.Forms.Button btnHideAll;
+    }
+}
---
+++ WinForms/VIZCore3DX.NET.LockedSelect/FrmMain.cs
@@ -0,0 +1,333 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace VIZCore3DX.NET.LockedSelect
+{
+    public partial class FrmMain : Form
+    {
+        // VIZCore3DX.NET 선언
+        private VIZCore3DX.NET.VIZCore3DXControl vizcore3dx;
+        private bool synchronizingVisibleState;
+
+        public FrmMain()
+        {
+            InitializeComponent();
+
+            // Initialize VIZCore3DX.NET
+            VIZCore3DX.NET.ModuleInitializer.Run();
+
+            // Construction
+            vizcore3dx = new VIZCore3DX.NET.VIZCore3DXControl();
+            vizcore3dx.Dock = DockStyle.Fill;
+            splitContainer1.Panel2.Controls.Add(vizcore3dx);
+
+            // Event
+            vizcore3dx.OnInitializedVIZCore3DX += VIZCore3DX_OnInitializedVIZCore3DX;
+        }
+
+        private void VIZCore3DX_OnInitializedVIZCore3DX(object sender, EventArgs e)
+        {
+            // ================================================================
+            // Example
+            // ================================================================
+
+            // 라이선스 파일을 통한 인증
+            //VIZCore3DX.NET.Data.LicenseResults result = vizcore3dx.License.LicenseFile("C:\\Temp\\VIZCore3DX.NET.lic");
+
+            // 라이선스 서버를 통한 인증
+            VIZCore3DX.NET.Data.LicenseResults result = vizcore3dx.License.LicenseServer("127.0.0.1", 8901);
+
+            // ================================================================
+            // License
+            // ================================================================
+
+            if (result != VIZCore3DX.NET.Data.LicenseResults.SUCCESS)
+            {
+                MessageBox.Show(string.Format("LICENSE CODE : {0}", result.ToString()), "VIZCore3DX.NET", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            InitializeVIZCore3DX();
+        }
+
+        private void InitializeVIZCore3DX()
+        {
+            vizcore3dx.Object3D.OnObject3DVisibleChangedEvent += Object3D_OnObject3DVisibleChangedEvent;
+
+            InitializeObjectList();
+        }
+
+        #region Locked Select
+
+        private void InitializeObjectList()
+        {
+            dgvLockedSelect.Rows.Clear();
+            UpdateCount();
+        }
+
+        private bool ContainsGridNode(VIZCore3DX.NET.Data.Node node)
+        {
+            if (node == null || node.IsValid == false) return false;
+
+            foreach (DataGridViewRow row in dgvLockedSelect.Rows)
+            {
+                VIZCore3DX.NET.Data.Node gridNode = row.Tag as VIZCore3DX.NET.Data.Node;
+
+                if (gridNode == null || gridNode.IsValid == false) continue;
+                if (gridNode.EntityID == node.EntityID && gridNode.Index == node.Index) return true;
+            }
+
+            return false;
+        }
+
+        private void AddGridNode(VIZCore3DX.NET.Data.Node node)
+        {
+            if (node == null || node.IsValid == false) return;
+            if (ContainsGridNode(node) == true) return;
+
+            int rowIndex = dgvLockedSelect.Rows.Add(node.NodeName, node.Kind, node.Visible);
+            dgvLockedSelect.Rows[rowIndex].Tag = node;
+        }
+
+        private List<VIZCore3DX.NET.Data.Node> GetSelectedGridNodes()
+        {
+            List<VIZCore3DX.NET.Data.Node> nodes = new List<VIZCore3DX.NET.Data.Node>();
+
+            foreach (DataGridViewRow row in dgvLockedSelect.SelectedRows)
+            {
+                VIZCore3DX.NET.Data.Node node = row.Tag as VIZCore3DX.NET.Data.Node;
+
+                if (node == null || node.IsValid == false) continue;
+
+                nodes.Add(node);
+            }
+
+            return nodes;
+        }
+
+        private List<VIZCore3DX.NET.Data.Node> GetAllGridNodes()
+        {
+            List<VIZCore3DX.NET.Data.Node> nodes = new List<VIZCore3DX.NET.Data.Node>();
+
+            foreach (DataGridViewRow row in dgvLockedSelect.Rows)
+            {
+                VIZCore3DX.NET.Data.Node node = row.Tag as VIZCore3DX.NET.Data.Node;
+
+                if (node == null || node.IsValid == false) continue;
+
+                nodes.Add(node);
+            }
+
+            return nodes;
+        }
+
+        private void UpdateCount()
+        {
+            lblCount.Text = dgvLockedSelect.Rows.Count.ToString();
+        }
+
+        private void UpdateVisibleRows(List<VIZCore3DX.NET.Data.Node> nodes, bool visible)
+        {
+            if (nodes == null || nodes.Count == 0) return;
+
+            synchronizingVisibleState = true;
+            dgvLockedSelect.SuspendLayout();
+
+            try
+            {
+                foreach (DataGridViewRow row in dgvLockedSelect.Rows)
+                {
+                    VIZCore3DX.NET.Data.Node gridNode = row.Tag as VIZCore3DX.NET.Data.Node;
+
+                    if (gridNode == null || gridNode.IsValid == false) continue;
+
+                    foreach (VIZCore3DX.NET.Data.Node node in nodes)
+                    {
+                        if (node == null || node.IsValid == false) continue;
+                        if (gridNode.EntityID != node.EntityID || gridNode.Index != node.Index) continue;
+
+                        bool currentVisible = Convert.ToBoolean(row.Cells[colVisible.Index].Value);
+
+                        if (currentVisible != visible) row.Cells[colVisible.Index].Value = visible;
+
+                        break;
+                    }
+                }
+            }
+            finally
+            {
+                dgvLockedSelect.ResumeLayout();
+                synchronizingVisibleState = false;
+            }
+        }
+
+        private void SetObjectVisible(List<VIZCore3DX.NET.Data.Node> nodes, bool visible)
+        {
+            if (nodes == null || nodes.Count == 0) return;
+
+            vizcore3dx.BeginUpdate();
+
+            try
+            {
+                vizcore3dx.Object3D.Show(nodes, visible);
+            }
+            finally
+            {
+                vizcore3dx.EndUpdate();
+            }
+        }
+
+        private void BtnOpenModel_Click(object sender, EventArgs e)
+        {
+            if (vizcore3dx.Model.OpenFileDialog() == false) return;
+
+            InitializeObjectList();
+        }
+
+        private void BtnAddSelected_Click(object sender, EventArgs e)
+        {
+            List<VIZCore3DX.NET.Data.Node> nodes = vizcore3dx.Object3D.FromFilter(VIZCore3DX.NET.Data.Object3dFilter.SELECTED_TOP);
+
+            if (nodes == null || nodes.Count == 0)
+            {
+                MessageBox.Show("View에서 개체를 선택하세요.", "Locked Select", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            List<VIZCore3DX.NET.Data.Node> targets = new List<VIZCore3DX.NET.Data.Node>();
+
+            foreach (VIZCore3DX.NET.Data.Node node in nodes)
+            {
+                if (node == null || node.IsValid == false) continue;
+                if (ContainsGridNode(node) == true) continue;
+
+                targets.Add(node);
+            }
+
+            if (targets.Count == 0) return;
+
+            vizcore3dx.Object3D.LockedSelect.Add(targets);
+
+            synchronizingVisibleState = true;
+            dgvLockedSelect.SuspendLayout();
+
+            try
+            {
+                foreach (VIZCore3DX.NET.Data.Node node in targets)
+                {
+                    AddGridNode(node);
+                }
+
+                UpdateCount();
+                dgvLockedSelect.ClearSelection();
+            }
+            finally
+            {
+                dgvLockedSelect.ResumeLayout();
+                synchronizingVisibleState = false;
+            }
+        }
+
+        private void BtnUnlockSelected_Click(object sender, EventArgs e)
+        {
+            List<VIZCore3DX.NET.Data.Node> nodes = GetSelectedGridNodes();
+
+            if (nodes.Count == 0) return;
+
+            vizcore3dx.Object3D.LockedSelect.Delete(nodes);
+            dgvLockedSelect.SuspendLayout();
+
+            try
+            {
+                foreach (DataGridViewRow row in dgvLockedSelect.SelectedRows)
+                {
+                    dgvLockedSelect.Rows.Remove(row);
+                }
+
+                UpdateCount();
+            }
+            finally
+            {
+                dgvLockedSelect.ResumeLayout();
+            }
+        }
+
+        private void BtnClearLock_Click(object sender, EventArgs e)
+        {
+            List<VIZCore3DX.NET.Data.Node> nodes = GetAllGridNodes();
+
+            if (nodes.Count == 0) return;
+
+            vizcore3dx.Object3D.LockedSelect.Delete(nodes);
+            dgvLockedSelect.Rows.Clear();
+            UpdateCount();
+        }
+
+        private void BtnShow_Click(object sender, EventArgs e)
+        {
+            SetObjectVisible(GetSelectedGridNodes(), true);
+        }
+
+        private void BtnHide_Click(object sender, EventArgs e)
+        {
+            SetObjectVisible(GetSelectedGridNodes(), false);
+        }
+
+        private void BtnShowAll_Click(object sender, EventArgs e)
+        {
+            SetObjectVisible(GetAllGridNodes(), true);
+        }
+
+        private void BtnHideAll_Click(object sender, EventArgs e)
+        {
+            SetObjectVisible(GetAllGridNodes(), false);
+        }
+
+        private void DgvLockedSelect_CurrentCellDirtyStateChanged(object sender, EventArgs e)
+        {
+            if (dgvLockedSelect.CurrentCell == null) return;
+            if (dgvLockedSelect.IsCurrentCellDirty == false) return;
+            if (dgvLockedSelect.CurrentCell.ColumnIndex != colVisible.Index) return;
+
+            dgvLockedSelect.CommitEdit(DataGridViewDataErrorContexts.Commit);
+        }
+
+        private void DgvLockedSelect_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if (synchronizingVisibleState == true) return;
+            if (e.RowIndex < 0) return;
+            if (e.ColumnIndex != colVisible.Index) return;
+
+            DataGridViewRow row = dgvLockedSelect.Rows[e.RowIndex];
+            VIZCore3DX.NET.Data.Node node = row.Tag as VIZCore3DX.NET.Data.Node;
+
+            if (node == null || node.IsValid == false) return;
+
+            bool visible = Convert.ToBoolean(row.Cells[colVisible.Index].Value);
+            List<VIZCore3DX.NET.Data.Node> nodes = new List<VIZCore3DX.NET.Data.Node> { node };
+
+            SetObjectVisible(nodes, visible);
+        }
+
+        private void Object3D_OnObject3DVisibleChangedEvent(object sender, VIZCore3DX.NET.Event.EventManager.Object3DVisibleChangedEventArgs e)
+        {
+            if (e == null || e.Node == null || e.Node.Count == 0) return;
+
+            List<VIZCore3DX.NET.Data.Node> nodes = new List<VIZCore3DX.NET.Data.Node>(e.Node);
+            bool visible = e.Visible;
+
+            if (IsDisposed == true || Disposing == true) return;
+
+            if (InvokeRequired == true)
+            {
+                BeginInvoke(new Action(() => UpdateVisibleRows(nodes, visible)));
+                return;
+            }
+
+            UpdateVisibleRows(nodes, visible);
+        }
+
+        #endregion
+    }
+}
