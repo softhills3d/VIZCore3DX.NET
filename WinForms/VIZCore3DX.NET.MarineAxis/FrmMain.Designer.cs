76604496353707a86438c5fcf4f673c2f7f87e23
yjkwak <yjkwak@softhills.net>
2026-09-09 14:22:29 +0900

    MarineAxis 예제 추가

---
+++ WinForms/VIZCore3DX.NET.MarineAxis/FrmMain.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace VIZCore3DX.NET.MarineAxis
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.chkMarineAxisVisible = new System.Windows.Forms.CheckBox();
+            this.btnOpenModel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.chkMarineAxisVisible);
+            this.splitContainer1.Panel1.Controls.Add(this.btnOpenModel);
+            this.splitContainer1.Size = new System.Drawing.Size(1000, 480);
+            this.splitContainer1.SplitterDistance = 200;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // chkMarineAxisVisible
+            // 
+            this.chkMarineAxisVisible.AutoSize = true;
+            this.chkMarineAxisVisible.Location = new System.Drawing.Point(12, 42);
+            this.chkMarineAxisVisible.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.chkMarineAxisVisible.Name = "chkMarineAxisVisible";
+            this.chkMarineAxisVisible.Size = new System.Drawing.Size(120, 16);
+            this.chkMarineAxisVisible.TabIndex = 2;
+            this.chkMarineAxisVisible.Text = "Marine Axis 표시";
+            this.chkMarineAxisVisible.UseVisualStyleBackColor = true;
+            this.chkMarineAxisVisible.CheckedChanged += new System.EventHandler(this.ChkMarineAxisVisible_CheckedChanged);
+            // 
+            // btnOpenModel
+            // 
+            this.btnOpenModel.Location = new System.Drawing.Point(12, 10);
+            this.btnOpenModel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnOpenModel.Name = "btnOpenModel";
+            this.btnOpenModel.Size = new System.Drawing.Size(176, 28);
+            this.btnOpenModel.TabIndex = 0;
+            this.btnOpenModel.Text = "모델 불러오기";
+            this.btnOpenModel.UseVisualStyleBackColor = true;
+            this.btnOpenModel.Click += new System.EventHandler(this.BtnOpenModel_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1000, 480);
+            this.Controls.Add(this.splitContainer1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "VIZCore3DX.NET - Marine Axis";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Button btnOpenModel;
+        private System.Windows.Forms.CheckBox chkMarineAxisVisible;
+    }
+}
---
+++ WinForms/VIZCore3DX.NET.MarineAxis/FrmMain.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace VIZCore3DX.NET.MarineAxis
+{
+    public partial class FrmMain : Form
+    {
+        // VIZCore3DX.NET 선언
+        private VIZCore3DX.NET.VIZCore3DXControl vizcore3dx;
+
+        public FrmMain()
+        {
+            InitializeComponent();
+
+            // Initialize VIZCore3DX.NET
+            VIZCore3DX.NET.ModuleInitializer.Run();
+
+            // Construction
+            vizcore3dx = new VIZCore3DX.NET.VIZCore3DXControl();
+            vizcore3dx.Dock = DockStyle.Fill;
+            splitContainer1.Panel2.Controls.Add(vizcore3dx);
+
+            // Event
+            vizcore3dx.OnInitializedVIZCore3DX += VIZCore3DX_OnInitializedVIZCore3DX;
+        }
+
+        private void VIZCore3DX_OnInitializedVIZCore3DX(object sender, EventArgs e)
+        {
+            // ================================================================
+            // Example
+            // ================================================================
+
+            // 라이선스 파일을 통한 인증
+            //VIZCore3DX.NET.Data.LicenseResults result = vizcore3dx.License.LicenseFile("C:\\Temp\\VIZCore3DX.NET.lic");
+
+            // 라이선스 서버를 통한 인증
+            VIZCore3DX.NET.Data.LicenseResults result = vizcore3dx.License.LicenseServer("127.0.0.1", 8901);
+
+            // ================================================================
+            // License
+            // ================================================================
+
+            if (result != VIZCore3DX.NET.Data.LicenseResults.SUCCESS)
+            {
+                MessageBox.Show(string.Format("LICENSE CODE : {0}", result.ToString()), "VIZCore3DX.NET", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+        }
+
+        private void BtnOpenModel_Click(object sender, EventArgs e)
+        {
+            vizcore3dx.Model.OpenFileDialog();
+            chkMarineAxisVisible.Checked = vizcore3dx.View.MarineAxis.Visible;
+        }
+
+        private void ChkMarineAxisVisible_CheckedChanged(object sender, EventArgs e)
+        {
+            vizcore3dx.View.MarineAxis.Visible = chkMarineAxisVisible.Checked;
+        }
+    }
+}
