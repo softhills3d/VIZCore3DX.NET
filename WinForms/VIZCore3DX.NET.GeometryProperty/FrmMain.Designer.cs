03dff61ab5016d70b568a06a5cade05f560230a6
yjkwak <yjkwak@softhills.net>
2026-09-10 09:45:23 +0900

    Geometry Property API 추가에 따른 예제 코드 수정

--- WinForms/VIZCore3DX.NET.GeometryProperty/FrmMain.Designer.cs
+++ WinForms/VIZCore3DX.NET.GeometryProperty/FrmMain.Designer.cs
@@ -17,24 +17,38 @@
             {
                 components.Dispose();
             }
+
             base.Dispose(disposing);
         }
 
         #region Windows Form 디자이너에서 생성한 코드
 
         /// <summary>
-        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 디자이너 지원에 필요한 메서드입니다.
         /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
         /// </summary>
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainerProperty = new System.Windows.Forms.SplitContainer();
+            this.grpReturnType = new System.Windows.Forms.GroupBox();
+            this.rdoGetGeometry = new System.Windows.Forms.RadioButton();
+            this.rdoGetCenterOfVolume = new System.Windows.Forms.RadioButton();
+            this.rdoGetVolume = new System.Windows.Forms.RadioButton();
+            this.rdoGetSurfaceArea = new System.Windows.Forms.RadioButton();
+            this.rdoFromSelectedObject3D = new System.Windows.Forms.RadioButton();
             this.geometryPropertyGrid = new System.Windows.Forms.PropertyGrid();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerProperty)).BeginInit();
+            this.splitContainerProperty.Panel1.SuspendLayout();
+            this.splitContainerProperty.Panel2.SuspendLayout();
+            this.splitContainerProperty.SuspendLayout();
+            this.grpReturnType.SuspendLayout();
             this.SuspendLayout();
+
             // 
             // splitContainer1
             // 
@@ -42,21 +56,125 @@
             this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel2;
             this.splitContainer1.Location = new System.Drawing.Point(0, 0);
             this.splitContainer1.Name = "splitContainer1";
+
             // 
             // splitContainer1.Panel2
             // 
-            this.splitContainer1.Panel2.Controls.Add(this.geometryPropertyGrid);
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainerProperty);
             this.splitContainer1.Size = new System.Drawing.Size(1088, 632);
             this.splitContainer1.SplitterDistance = 811;
             this.splitContainer1.TabIndex = 0;
+
+            // 
+            // splitContainerProperty
+            // 
+            this.splitContainerProperty.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainerProperty.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainerProperty.Location = new System.Drawing.Point(0, 0);
+            this.splitContainerProperty.Name = "splitContainerProperty";
+            this.splitContainerProperty.Orientation = System.Windows.Forms.Orientation.Horizontal;
+
+            // 
+            // splitContainerProperty.Panel1
+            // 
+            this.splitContainerProperty.Panel1.Controls.Add(this.grpReturnType);
+
+            // 
+            // splitContainerProperty.Panel2
+            // 
+            this.splitContainerProperty.Panel2.Controls.Add(this.geometryPropertyGrid);
+            this.splitContainerProperty.Size = new System.Drawing.Size(273, 632);
+            this.splitContainerProperty.SplitterDistance = 125;
+            this.splitContainerProperty.TabIndex = 0;
+
+            // 
+            // grpReturnType
+            // 
+            this.grpReturnType.Controls.Add(this.rdoGetGeometry);
+            this.grpReturnType.Controls.Add(this.rdoGetCenterOfVolume);
+            this.grpReturnType.Controls.Add(this.rdoGetVolume);
+            this.grpReturnType.Controls.Add(this.rdoGetSurfaceArea);
+            this.grpReturnType.Controls.Add(this.rdoFromSelectedObject3D);
+            this.grpReturnType.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grpReturnType.Location = new System.Drawing.Point(0, 0);
+            this.grpReturnType.Name = "grpReturnType";
+            this.grpReturnType.Padding = new System.Windows.Forms.Padding(8);
+            this.grpReturnType.Size = new System.Drawing.Size(273, 125);
+            this.grpReturnType.TabIndex = 0;
+            this.grpReturnType.TabStop = false;
+            this.grpReturnType.Text = "Geometry Property";
+
+            // 
+            // rdoFromSelectedObject3D
+            // 
+            this.rdoFromSelectedObject3D.AutoSize = true;
+            this.rdoFromSelectedObject3D.Checked = true;
+            this.rdoFromSelectedObject3D.Location = new System.Drawing.Point(12, 22);
+            this.rdoFromSelectedObject3D.Name = "rdoFromSelectedObject3D";
+            this.rdoFromSelectedObject3D.Size = new System.Drawing.Size(154, 16);
+            this.rdoFromSelectedObject3D.TabIndex = 0;
+            this.rdoFromSelectedObject3D.TabStop = true;
+            this.rdoFromSelectedObject3D.Text = "FromSelectedObject3D";
+            this.rdoFromSelectedObject3D.UseVisualStyleBackColor = true;
+            this.rdoFromSelectedObject3D.CheckedChanged += new System.EventHandler(this.GeometryReturnType_CheckedChanged);
+
+            // 
+            // rdoGetSurfaceArea
+            // 
+            this.rdoGetSurfaceArea.AutoSize = true;
+            this.rdoGetSurfaceArea.Location = new System.Drawing.Point(12, 46);
+            this.rdoGetSurfaceArea.Name = "rdoGetSurfaceArea";
+            this.rdoGetSurfaceArea.Size = new System.Drawing.Size(111, 16);
+            this.rdoGetSurfaceArea.TabIndex = 1;
+            this.rdoGetSurfaceArea.Text = "GetSurfaceArea";
+            this.rdoGetSurfaceArea.UseVisualStyleBackColor = true;
+            this.rdoGetSurfaceArea.CheckedChanged += new System.EventHandler(this.GeometryReturnType_CheckedChanged);
+
+            // 
+            // rdoGetVolume
+            // 
+            this.rdoGetVolume.AutoSize = true;
+            this.rdoGetVolume.Location = new System.Drawing.Point(143, 46);
+            this.rdoGetVolume.Name = "rdoGetVolume";
+            this.rdoGetVolume.Size = new System.Drawing.Size(84, 16);
+            this.rdoGetVolume.TabIndex = 2;
+            this.rdoGetVolume.Text = "GetVolume";
+            this.rdoGetVolume.UseVisualStyleBackColor = true;
+            this.rdoGetVolume.CheckedChanged += new System.EventHandler(this.GeometryReturnType_CheckedChanged);
+
+            // 
+            // rdoGetCenterOfVolume
+            // 
+            this.rdoGetCenterOfVolume.AutoSize = true;
+            this.rdoGetCenterOfVolume.Location = new System.Drawing.Point(12, 70);
+            this.rdoGetCenterOfVolume.Name = "rdoGetCenterOfVolume";
+            this.rdoGetCenterOfVolume.Size = new System.Drawing.Size(133, 16);
+            this.rdoGetCenterOfVolume.TabIndex = 3;
+            this.rdoGetCenterOfVolume.Text = "GetCenterOfVolume";
+            this.rdoGetCenterOfVolume.UseVisualStyleBackColor = true;
+            this.rdoGetCenterOfVolume.CheckedChanged += new System.EventHandler(this.GeometryReturnType_CheckedChanged);
+
+            // 
+            // rdoGetGeometry
+            // 
+            this.rdoGetGeometry.AutoSize = true;
+            this.rdoGetGeometry.Location = new System.Drawing.Point(12, 94);
+            this.rdoGetGeometry.Name = "rdoGetGeometry";
+            this.rdoGetGeometry.Size = new System.Drawing.Size(94, 16);
+            this.rdoGetGeometry.TabIndex = 4;
+            this.rdoGetGeometry.Text = "GetGeometry";
+            this.rdoGetGeometry.UseVisualStyleBackColor = true;
+            this.rdoGetGeometry.CheckedChanged += new System.EventHandler(this.GeometryReturnType_CheckedChanged);
+
             // 
             // geometryPropertyGrid
             // 
             this.geometryPropertyGrid.Dock = System.Windows.Forms.DockStyle.Fill;
             this.geometryPropertyGrid.Location = new System.Drawing.Point(0, 0);
             this.geometryPropertyGrid.Name = "geometryPropertyGrid";
-            this.geometryPropertyGrid.Size = new System.Drawing.Size(273, 632);
+            this.geometryPropertyGrid.Size = new System.Drawing.Size(273, 503);
             this.geometryPropertyGrid.TabIndex = 0;
+
             // 
             // FrmMain
             // 
@@ -71,14 +189,28 @@
             this.splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
+            this.splitContainerProperty.Panel1.ResumeLayout(false);
+            this.splitContainerProperty.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerProperty)).EndInit();
+            this.splitContainerProperty.ResumeLayout(false);
+            this.grpReturnType.ResumeLayout(false);
+            this.grpReturnType.PerformLayout();
             this.ResumeLayout(false);
-
         }
 
         #endregion
 
         private System.Windows.Forms.SplitContainer splitContainer1;
+
+        // 우측 영역을 조회 방식 / 결과 영역으로 분할
+        private System.Windows.Forms.SplitContainer splitContainerProperty;
+        private System.Windows.Forms.GroupBox grpReturnType;
+        private System.Windows.Forms.RadioButton rdoFromSelectedObject3D;
+        private System.Windows.Forms.RadioButton rdoGetSurfaceArea;
+        private System.Windows.Forms.RadioButton rdoGetVolume;
+        private System.Windows.Forms.RadioButton rdoGetCenterOfVolume;
+        private System.Windows.Forms.RadioButton rdoGetGeometry;
+
         private System.Windows.Forms.PropertyGrid geometryPropertyGrid;
     }
 }
-
--- WinForms/VIZCore3DX.NET.GeometryProperty/FrmMain.cs
+++ WinForms/VIZCore3DX.NET.GeometryProperty/FrmMain.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows.Forms;
 using VIZCore3DX.NET.Event;
 
@@ -21,7 +22,7 @@ namespace VIZCore3DX.NET.GeometryProperty
             vizcore3dx.Dock = DockStyle.Fill;
             splitContainer1.Panel1.Controls.Add(vizcore3dx);
 
-            //Event
+            // Event
             vizcore3dx.OnInitializedVIZCore3DX += VIZCore3DX_OnInitializedVIZCore3DX;
         }
 
@@ -30,8 +31,9 @@ namespace VIZCore3DX.NET.GeometryProperty
             // ================================================================
             // Example
             // ================================================================
+
             // 라이선스 파일을 통한 인증
-            //vizcore3dx.License.LicenseFile("C:\\Temp\\VIZCore3DX.NET.lic");
+            //VIZCore3DX.NET.Data.LicenseResults result = vizcore3dx.License.LicenseFile("C:\\Temp\\VIZCore3DX.NET.lic");
 
             // 라이선스 서버를 통한 인증
             VIZCore3DX.NET.Data.LicenseResults result = vizcore3dx.License.LicenseServer("127.0.0.1", 8901);
@@ -39,15 +41,14 @@ namespace VIZCore3DX.NET.GeometryProperty
             // ================================================================
             // License
             // ================================================================
-            // VIZCore3DX.NET.Data.LicenseResults result = vizcore3dx.License.LicenseFile("C:\\License\\VIZCore3DX.NET.lic");
+
             if (result != VIZCore3DX.NET.Data.LicenseResults.SUCCESS)
             {
-                MessageBox.Show(string.Format("LICENSE CODE : {0}", result.ToString()), "VIZCore3DX.NET", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                MessageBox.Show(string.Format("LICENSE CODE : {0}", result), "VIZCore3DX.NET", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return;
             }
 
             InitializeVIZCore3DX();
-            InitializeVIZCore3DXEvent();
         }
 
         private void InitializeVIZCore3DX()
@@ -70,26 +71,75 @@ namespace VIZCore3DX.NET.GeometryProperty
             // 모델 열기 시, 3D 화면 Rendering 재시작
             // ================================================================
             vizcore3dx.EndUpdate();
+
+            // 마우스로 개체 선택 상태가 변경됐을 때
+            vizcore3dx.Object3D.OnNodeEvent += Object3D_OnNodeEvent;
         }
 
-        private void InitializeVIZCore3DXEvent()
+        #region Geometry Property
+
+        private void Object3D_OnNodeEvent(object sender, EventManager.NodeEventArgs e)
         {
-            // 마우스로 개체 클릭했을 때
-            vizcore3dx.Object3D.OnNodeClick += Object3D_OnNodeClick;
+            if (e == null || e.EventKind != VIZCore3DX.NET.Manager.Object3DManager.NodeEventKind.SELECTION_CHANGED_NODE) return;
+
+            UpdateGeometryProperty();
         }
 
-        private void Object3D_OnNodeClick(
-            object sender,
-            EventManager.NodeMouseEventArgs e)
+        private void GeometryReturnType_CheckedChanged(object sender, EventArgs e)
         {
-            if (e == null || e.Node == null)
+            RadioButton radioButton = sender as RadioButton;
+            if (radioButton == null || radioButton.Checked == false) return;
+
+            UpdateGeometryProperty();
+        }
+
+        private void UpdateGeometryProperty()
+        {
+            List<VIZCore3DX.NET.Data.Node> selectedNodes = vizcore3dx.Object3D.FromFilter(VIZCore3DX.NET.Data.Object3dFilter.SELECTED_TOP);
+
+            if (selectedNodes.Count == 0)
             {
                 geometryPropertyGrid.SelectedObject = null;
                 return;
             }
 
-            VIZCore3DX.NET.Data.Objects3DProperty prop = vizcore3dx.Object3D.GeometryProperty.FromSelectedObject3D(false);
-            geometryPropertyGrid.SelectedObject = prop;
+            // Geometry Property
+            if (rdoFromSelectedObject3D.Checked)
+            {
+                geometryPropertyGrid.SelectedObject = vizcore3dx.Object3D.GeometryProperty.FromSelectedObject3D(false);
+                return;
+            }
+
+            // 표면적 (m²)
+            if (rdoGetSurfaceArea.Checked)
+            {
+                double surfaceArea = vizcore3dx.Object3D.GeometryProperty.GetSurfaceArea(selectedNodes);
+                geometryPropertyGrid.SelectedObject = new { SurfaceArea = surfaceArea, Unit = "m²" };
+                return;
+            }
+
+            // 부피 (m³)
+            if (rdoGetVolume.Checked)
+            {
+                double volume = vizcore3dx.Object3D.GeometryProperty.GetVolume(selectedNodes);
+                geometryPropertyGrid.SelectedObject = new { Volume = volume, Unit = "m³" };
+                return;
+            }
+
+            // 부피 중심 (mm)
+            if (rdoGetCenterOfVolume.Checked)
+            {
+                geometryPropertyGrid.SelectedObject = vizcore3dx.Object3D.GeometryProperty.GetCenterOfVolume(selectedNodes);
+                return;
+            }
+
+            // 표면적 + 부피 + 부피 중심
+            if (rdoGetGeometry.Checked)
+            {
+                geometryPropertyGrid.SelectedObject = vizcore3dx.Object3D.GeometryProperty.GetGeometry(selectedNodes);
+            }
         }
+
+        #endregion
     }
 }
